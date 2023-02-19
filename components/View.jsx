fec5dee339b8ba1eec41df7ef769d5ae1db6ae96
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-02-19 12:16:19 +0000

    Sun, Feb 19, 2023, 12:16 PM +00:00

--- components/View.jsx
+++ components/View.jsx
@@ -166,7 +166,7 @@ function AttendanceViewer() {
   };
 
   return (
-    <div className="h-screen flex flex-column items-center">
+    <div className="h-screen flex flex-colum items-center">
       <div className="bg-white p-8 rounded-lg shadow-lg">
         <h2 className="text-gray-500 text-xl font-bold mb-4">Today's Attendance</h2>
         <ul className="divide-y divide-gray-200">
