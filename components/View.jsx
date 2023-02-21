c88471d5671f6204c9ed0bc56be18f200788892d
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-02-21 02:46:12 +0000

    Tue, Feb 21, 2023, 2:46 AM +00:00

--- components/View.jsx
+++ components/View.jsx
@@ -63,67 +63,11 @@ function AttendanceViewer() {
     setSelectedDate(date);
   };
 
-  return (
-    <div className="h-screen flex flex-col items-center">
-      <div className="bg-white p-8 rounded-lg shadow-lg">
-        <h2 className="text-gray-700 text-xl font-bold mb-4">Attendance For Today</h2>
-        <ul className="divide-y divide-gray-200">
-          {todayAttendance.map((student) => (
-            <li key={student.id} className="py-4">
-              <div className="flex items-center justify-between">
-                <div>
-                  <h3 className="text-gray-700 text-lg font-bold">{student.name}</h3>
-                  <p className="text-gray-700">
-                    {student.lastScan
-                      ? `Last Scan: ${student.lastScan.toLocaleTimeString()}`
-                      : "Not yet scanned"}
-                  </p>
-                </div>
-                <span className="text-gray-700">{}</span>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
-    <div className="bg-white p-8 rounded-lg shadow-lg mt-8">
-  <h2 className="text-gray-700 text-xl font-bold mb-4">Past Attendance</h2>
-  <div className="flex items-center mb-4">
-    <label htmlFor="attendanceDate" className="block mr-4 font-medium text-gray-700">
-      Select a date:
-    </label>
-    <input
-      type="date"
-      id="attendanceDate"
-      name="attendanceDate"
-      value={selectedDate.toISOString().slice(0, 10)}
-      onChange={handleDateChange}
-      className=" text-gray-700 border border-gray-400 px-2 py-1 rounded-lg"
-    />
-  </div>
-  {pastAttendance.length > 0 ? (
-    <ul className="divide-y divide-gray-200">
-      {pastAttendance.map((student) => (
-        <li key={student.id} className="py-4">
-          <div className="flex items-center justify-between">
-            <div>
-              <h3 className="text-gray-700 text-lg font-bold">{student.name}</h3>
-              <p className="text-gray-700">
-                {student.lastScan
-                  ? `Last Scan: ${student.lastScan.toLocaleTimeString()}`
-                  : "Not yet scanned"}
-              </p>
-            </div>
-          </div>
-        </li>
-      ))}
-    </ul>
-  ) : (
-    <p className="text-gray-700">No attendance records found for this date.</p>
-  )}
+  return
+  (
 
-</div>
-</div>
-)
+  )
+
 }
 
 
