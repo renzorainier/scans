fa52b86eb926d9566a5fb60900d410450be2d82e
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-02-21 11:08:00 +0000

    Tue, Feb 21, 2023, 11:08 AM +00:00

--- components/TodayView.jsx
+++ components/TodayView.jsx
@@ -36,8 +36,8 @@ function TodayAttendance() {
   return (
     <div className="bg-white p-8 rounded-lg shadow-lg">
       <h2 className="text-gray-700 text-xl font-bold mb-4">Attendance For Today</h2>
-      <table className="w-full divide-y divide-gray-200 ">
-        <thead className="bg-gray-50">
+      <table className="w-full divide-y  divide-gray-200 ">
+        <thead className="bg-gray-50 ">
           <tr>
             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
               Name
