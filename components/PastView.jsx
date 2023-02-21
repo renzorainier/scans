036b47897ff99da41042ab290a920fc4ac966794
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-02-21 10:34:47 +0000

    Tue, Feb 21, 2023, 10:34 AM +00:00

--- components/PastView.jsx
+++ components/PastView.jsx
@@ -9,53 +9,33 @@ import {
 } from "firebase/firestore";
 import { db } from "./firebase.js";
 
-function TodayAttendance() {
-  const [todayAttendance, setTodayAttendance] = useState([]);
+function PastAttendance() {
+  const [pastAttendance, setPastAttendance] = useState([]);
+  const [selectedDate, setSelectedDate] = useState(new Date());
 
   useEffect(() => {
-    const fetchTodayAttendance = async () => {
-      const presentStudentsQuery = query(
-        collection(db, "strands", "STEM", "1B"),
-        where("present", "==", true)
-      );
-      const presentStudentsQuerySnapshot = await getDocs(presentStudentsQuery);
-      const presentStudents = presentStudentsQuerySnapshot.docs.map((doc) => ({
-        id: doc.id,
-        name: doc.data().name,
-        lastScan: doc.data().lastScan?.toDate() || null,
-        section: doc.data().section,
-      }));
-      presentStudents.sort((a, b) => b.lastScan - a.lastScan);
-      setTodayAttendance(presentStudents);
+    const fetchPastAttendance = async () => {
+      const dateStr = selectedDate.toISOString().slice(0, 10);
+      const attendanceDocRef = doc(db, "presentStudents", dateStr);
+      const attendanceDocSnapshot = await getDoc(attendanceDocRef);
+      if (attendanceDocSnapshot.exists()) {
+        const attendanceData = attendanceDocSnapshot.data();
+        const presentStudents = attendanceData.presentStudents.map(
+          (student) => ({
+            ...student,
+            lastScan: student.lastScan.toDate(),
+            section: student.section,
+          })
+        );
+        presentStudents.sort((a, b) => b.lastScan - a.lastScan);
+        setPastAttendance(presentStudents);
+      } else {
+        setPastAttendance([]);
+      }
     };
 
-    fetchTodayAttendance();
-  }, []);
-
-  return (
-    <div className="bg-white p-8 rounded-lg shadow-lg">
-      <h2 className="text-gray-700 text-xl font-bold mb-4">Attendance For Today</h2>
-      <ul className="divide-y divide-gray-200">
-        {todayAttendance.map((student) => (
-          <li key={student.id} className="py-4 divide-x divide-gray-200">
-            <div className="flex items-center justify-between">
-              <div className="flex flex-row items-center justify-between">
-                <h3 className="text-gray-700 text-lg font-bold">{student.name}</h3>
-                <p className="text-gray-700">
-                  {student.lastScan
-                    ? `Last Scan: ${student.lastScan.toLocaleTimeString()}`
-                    : "Not yet scanned"}
-                </p>
-                <p className="text-gray-800">{student.section}</p>
-              </div>
-              <span className="text-gray-700">{}</span>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+    fetchPastAttendance();
+  }, [selectedDate]);
 
 
 
