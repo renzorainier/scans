eb9110346a200f5d47fec825f873bd7c8174b2e9
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-02-21 13:13:08 +0000

    Tue, Feb 21, 2023, 1:13 PM +00:00

--- components/TodayView.jsx
+++ components/TodayView.jsx
@@ -39,13 +39,13 @@ function TodayAttendance() {
       <table className="w-full divide-y divide-gray-200" style={{ tableLayout: 'fixed' }}>
         <thead className="bg-gray-50">
           <tr>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider text-sm">
+            <th className="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider text-sm">
               Name
             </th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider text-sm">
+            <th className="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider text-sm">
               Last Scan
             </th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider text-sm">
+            <th className="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider text-sm">
               Section
             </th>
           </tr>
