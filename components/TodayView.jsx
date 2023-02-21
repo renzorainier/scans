1e4fbd9e2016a34130a72afeb1cdb776f8b852db
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-02-21 13:13:53 +0000

    Tue, Feb 21, 2023, 1:13 PM +00:00

--- components/TodayView.jsx
+++ components/TodayView.jsx
@@ -34,35 +34,37 @@ function TodayAttendance() {
   }, []);
 
   return (
-    <div className="bg-white p-8 divide-x divide-y rounded-lg shadow-lg">
+    <div className="bg-white p-8 divide-x  divide-y rounded-lg shadow-lg">
       <h2 className="text-gray-700 text-xl font-bold mb-4">Attendance For Today</h2>
-      <table className="w-full divide-y divide-gray-200" style={{ tableLayout: 'fixed' }}>
-        <thead className="bg-gray-50">
+      <table className="w-full divide-y  divide-gray-200 ">
+        <thead className="bg-gray-50 ">
           <tr>
-            <th className="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider text-sm">
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
               Name
             </th>
-            <th className="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider text-sm">
+            <th className="px-6 py-3 text-left  text-xs font-medium text-gray-500 uppercase tracking-wider">
               Last Scan
             </th>
-            <th className="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider text-sm">
+            <th className="px-6 py-3 text-left  text-xs font-medium text-gray-500 uppercase tracking-wider">
               Section
             </th>
           </tr>
         </thead>
-        <tbody className="bg-white divide-y divide-gray-200">
+        <tbody className="bg-white  divide-y divide-gray-200">
           {todayAttendance.map((student) => (
             <tr key={student.id}>
               <td className="px-6 py-4 whitespace-nowrap">
-                <div className="flex items-center">
-                  <div className="ml-4">
+                <div className="flex items-center ">
+                  <div className="ml-4 ">
                     <div className="text-sm font-medium text-gray-900">{student.name}</div>
                   </div>
                 </div>
               </td>
-              <td className="px-6 py-4 whitespace-nowrap">
+              <td className="px-6 py-4 whitespace-nowrap ">
                 <div className="text-sm text-gray-900">
-                  {student.lastScan ? student.lastScan.toLocaleTimeString() : "No Record"}
+                  {student.lastScan
+                    ? student.lastScan.toLocaleTimeString()
+                    : "No Record"}
                 </div>
               </td>
               <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{student.section}</td>
@@ -72,11 +74,9 @@ function TodayAttendance() {
       </table>
     </div>
   );
-
 }
 
 export default TodayAttendance;
-
 
 
 
