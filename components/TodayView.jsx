9bf91ad37940cc0c6f13defd96252fac04ad0b1e
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-02-22 15:05:31 +0000

    Wed, Feb 22, 2023, 3:05 PM +00:00

--- components/TodayView.jsx
+++ components/TodayView.jsx
@@ -19,7 +19,7 @@ function TodayAttendance() {
 
   useEffect(() => {
     const fetchTodayAttendance = async () => {
-      let attendanceQuery = collection(db,"strands", "STEM", "1B");
+      let attendanceQuery = collection(db,"strands");
 
       if (selectedStrand) {
         attendanceQuery = query(
