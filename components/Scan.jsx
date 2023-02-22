61335f051f29f56f79d7ddd42bb50b6ed2916b95
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-02-22 12:03:19 +0000

    Wed, Feb 22, 2023, 12:03 PM +00:00

--- components/Scan.jsx
+++ components/Scan.jsx
@@ -2,12 +2,9 @@ import React, { useState, useEffect } from "react";
 import { QrReader } from "react-qr-reader";
 import {
   collection,
-  query,
-  where,
-  getDocs,
+  doc,
   getDoc,
   setDoc,
-  doc,
 } from "firebase/firestore";
 import { db } from "./firebase.js";
 
@@ -24,39 +21,49 @@ function Scan() {
   }, [data]);
 
   const handleMarkPresent = async (strand, section, id) => {
-    try {
-      const studentRef = doc(db, "strands", strand, section, id);
-      const docSnapshot = await getDoc(studentRef);
-      if (docSnapshot.exists()) {
-        const studentData = docSnapshot.data();
-        if (!studentData.present) {
-          await setDoc(
-            studentRef,
-            { present: true, lastScan: new Date() },
-            { merge: true }
-          );
-          console.log(`Student ${id} marked as present`);
-        } else {
-          console.log(`Student ${id} is already marked as present`);
-        }
+    const studentRef = doc(
+      db,
+      "strands",
+      strand,
+      section,
+      id
+    );
+    const docSnapshot = await getDoc(studentRef);
+    if (docSnapshot.exists()) {
+      const studentData = docSnapshot.data();
+      if (!studentData.present) {
+        await setDoc(
+          studentRef,
+          { present: true, lastScan: new Date() },
+          { merge: true }
+        );
+        console.log(`Student ${id} marked as present`);
+      } else {
+        console.log(`Student ${id} is already marked as present`);
+      }
 
-        const timeString = studentData.lastScan
+      // const timeString = studentData.lastScan
+      // ? studentData.lastScan.toDate().toLocaleTimeString("en-US", {
+      //     hour: "numeric",
+      //     minute: "numeric",
+      //     hour12: true,
+      //   })
+      // : "";
+      const timeString = studentData.lastScan
           .toDate()
           .toLocaleTimeString("en-US", {
             hour: "numeric",
             minute: "numeric",
             hour12: true,
           });
-        return {
-          name: studentData.name,
-          time: timeString,
-        };
-      } else {
-        console.log(`No student found with ID ${id}`);
-        return undefined;
-      }
-    } catch (e) {
-      console.error("Error marking student as present: ", e);
+
+      return {
+        name: studentData.name,
+        time: timeString,
+      };
+    } else {
+      console.log(`No student found with ID ${id}`);
+      return undefined;
     }
   };
 
@@ -88,25 +95,27 @@ function Scan() {
         onResult={async (result) => {
           if (!!result) {
             const code = result.text;
-            if (code !== lastScanned) {
+            const [strand, section, id] = code.split("-");
+            if (strand && section && id && code !== lastScanned) {
               setLastScanned(code);
-              const parts = code.split("-");
-              if (parts.length === 3) {
-                const [strand, section, id] = parts;
-                const studentInfo = await handleMarkPresent(strand, section, id);
-                if (studentInfo) {
-                  const { name, time } = studentInfo;
-                  setData(`Name: ${name}, Scanned at: ${time}`);
-                }
-              } else {
-                console.log(`Invalid QR code format: ${code}`);
+              const studentInfo = await handleMarkPresent(
+                strand,
+                section,
+                id
+              );
+              if (studentInfo) {
+                const { name, time } = studentInfo;
+                setData(`Name: ${name}, Scanned at: ${time}`);
               }
             }
           }
         }}
+        // This is facing mode: "environment". It will open the back camera of
+        // the smartphone and if not found, will open the front camera
         constraints={{ facingMode: "environment" }}
         style={{ width: "100%", height: "100%" }}
       />
+
       <p className="text-xl font-bold mt-6">Scan result:</p>
       <p className="text-xl">{data}</p>
       <h1 className="text-3xl font-semibold mt-8">Recent Logs</h1>
