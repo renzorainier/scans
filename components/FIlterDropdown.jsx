bb16c18437dbf3b4fd4f449646cd8b347f69d949
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-02-23 00:40:36 +0000

    Thu, Feb 23, 2023, 12:40 AM +00:00

--- components/FIlterDropdown.jsx
+++ components/FIlterDropdown.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 
-function FilterDropdown({ options, value, onChange }) {
+function FilterDropdown({ label, options, selected, handleFilterChange }) {
   return (
-<select value={value} onChange={(e) => handleFilterChange(e.target.value)}>      {options.map((option) => (
-        <option key={option} value={option}>
-          {option}
-        </option>
-      ))}
-    </select>
+    <div className="filter-dropdown">
+      <label>{label}</label>
+      <select value={selected} onChange={(e) => handleFilterChange(e.target.value)}>
+        {options.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </div>
   );
 }
 
