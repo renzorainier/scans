a776301ee6842955d349763687480fcd17482d28
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-02-24 16:12:03 +0000

    Fri, Feb 24, 2023, 4:12 PM +00:00

--- components/Tab.jsx
+++ components/Tab.jsx
@@ -36,6 +36,9 @@ function TabSelector() {
           enter="transition-opacity duration-500"
           enterFrom="opacity-0"
           enterTo="opacity-100"
+          leave="transition-opacity duration-500"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
         >
           <TodayAttendance />
         </Transition>
