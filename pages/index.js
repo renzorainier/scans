1126f7152a0e146f6e29efec11bdeff1ddfcd755
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-02-24 02:09:51 +0000

    Fri, Feb 24, 2023, 2:09 AM +00:00

--- pages/index.js
+++ pages/index.js
@@ -102,8 +102,10 @@ export default function Home() {
         <meta name="description" content="Welcome to Scas Attendance Scan" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400&amp;display=swap" rel="stylesheet"/>
+
       </Head>
-      <main className="container mx-auto  ">{renderComponent()}</main>
+      <main className="container mx-auto ">{renderComponent()}</main>
     </>
   );
 }
