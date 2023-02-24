2034dce861a461c3eda0761a4720490954de5108
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-02-24 02:18:39 +0000

    Fri, Feb 24, 2023, 2:18 AM +00:00

--- components/Sample.jsx
+++ components/Sample.jsx
@@ -2,6 +2,33 @@ import React from 'react'
 
 function Sample() {
   return (
+    let tabs = document.querySelectorAll(".tab")
+    let indicator = document.querySelector(".indicator")
+    let panels = document.querySelectorAll(".tab-panel")
+
+    indicator.style.width = tabs[0].getBoundingClientRect().width + 'px'
+    indicator.style.left = tabs[0].getBoundingClientRect().left - tabs[0].parentElement.getBoundingClientRect().left + 'px'
+
+    tabs.forEach(tab =>{
+      tab.addEventListener("click", ()=>{
+        let tabTarget = tab.getAttribute("aria-controls")
+
+        indicator.style.width = tab.getBoundingClientRect().width + 'px'
+        indicator.style.left = tab.getBoundingClientRect().left - tab.parentElement.getBoundingClientRect().left + 'px'
+
+
+        panels.forEach(panel =>{
+          let panelId = panel.getAttribute("id")
+          if(tabTarget === panelId){
+            panel.classList.remove("invisible", "opacity-0")
+            panel.classList.add("visible", "opacity-100")
+          } else {
+            panel.classList.add("invisible", "opacity-0")
+          }
+        })
+      })
+    })
+
     <div>
       <body class="min-h-screen flex items-center bg-gradient-to-br from-purple-200 to-indigo-400">
     <div class="max-w-3xl mx-auto px-8 sm:px-0">
