19bed7b0130b5d9867c6d09d62e783e433d1a987
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-02-24 02:43:28 +0000

    Fri, Feb 24, 2023, 2:43 AM +00:00

--- components/Sample.jsx
+++ components/Sample.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Tabs = () => {
-const [activeTab, setActiveTab] = useState(1);
+const [activeTab, setActiveTab] = useState("panel-1");
 
-const handleTabClick = (tabIndex) => {
-setActiveTab(tabIndex);
+const handleClick = (event, tabId) => {
+event.preventDefault();
+setActiveTab(tabId);
 };
 
 return (
@@ -13,48 +14,48 @@ return (
 <div
        role="tablist"
        aria-label="tabs"
-       className="relative w-max mx-auto h-12 grid grid-cols-2 items-center px-[3px] rounded-full bg-gray-900/20 overflow-hidden shadow-2xl shadow-900/20 transition"
+       className="relative w-max mx-auto h-12 grid grid-cols-3 items-center px-[3px] rounded-full bg-gray-900/20 overflow-hidden shadow-2xl shadow-900/20 transition"
      >
-<div className="absolute indicator h-11 my-auto top-0 bottom-0 left-0 rounded-full bg-white shadow-md" style={{ width: ${document.querySelector(".tab").getBoundingClientRect().width}px, left: ${document.querySelector(".tab").getBoundingClientRect().left - document.querySelector(".tab").parentElement.getBoundingClientRect().left}px}}></div>
+<div className="absolute indicator h-11 my-auto top-0 bottom-0 left-0 rounded-full bg-white shadow-md"></div>
 <button
 role="tab"
-aria-selected={activeTab === 1}
+aria-selected={activeTab === "panel-1"}
 aria-controls="panel-1"
 id="tab-1"
 tabIndex="0"
-className={relative block h-10 px-6 tab rounded-full ${activeTab === 1 ? 'bg-gray-900 text-white' : 'text-gray-800'}}
-onClick={() => handleTabClick(1)}
+className="relative block h-10 px-6 tab rounded-full"
+onClick={(event) => handleClick(event, "panel-1")}
 >
-<span>First Tab</span>
+<span className="text-gray-800">First Tab</span>
 </button>
 <button
 role="tab"
-aria-selected={activeTab === 2}
+aria-selected={activeTab === "panel-2"}
 aria-controls="panel-2"
 id="tab-2"
 tabIndex="-1"
-className={relative block h-10 px-6 tab rounded-full ${activeTab === 2 ? 'bg-gray-900 text-white' : 'text-gray-800'}}
-onClick={() => handleTabClick(2)}
+className="relative block h-10 px-6 tab rounded-full"
+onClick={(event) => handleClick(event, "panel-2")}
 >
-<span>Second Tab</span>
+<span className="text-gray-800">Second Tab</span>
 </button>
 </div>
 <div className="mt-6 relative rounded-3xl bg-purple-50">
 <div
 role="tabpanel"
 id="panel-1"
-className={tab-panel p-6 transition duration-300 ${activeTab === 1 ? 'visible opacity-100' : 'invisible opacity-0'}}
+className={tab-panel p-6 transition duration-300 ${ activeTab === "panel-1" ? "visible opacity-100" : "invisible opacity-0" }}
 >
 <h2 className="text-xl font-semibold text-gray-800">First tab panel</h2>
-<p className="mt-4 text-gray-600">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas dolores voluptate temporibus, atque ab eos, delectus at ad hic voluptatem veritatis iure, nulla voluptates quod nobis doloremque eaque! Perferendis, soluta.</p>
+<p className="mt-4 text-gray-600">Lorem</p>
 </div>
 <div
 role="tabpanel"
 id="panel-2"
-className={tab-panel p-6 transition duration-300 ${activeTab === 2 ? 'visible opacity-100' : 'invisible opacity-0'}}
+className={absolute top-0 tab-panel p-6 transition duration-300 ${ activeTab === "panel-2" ? "visible opacity-100" : "invisible opacity-0" }}
 >
 <h2 className="text-xl font-semibold text-gray-800">Second tab panel</h2>
-<p className="mt-4 text-gray-600">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas dolores voluptate temporibus, atque ab eos, delectus at ad hic voluptatem veritatis iure, nulla voluptates quod nobis doloremque eaque! Perferendis, soluta.</p>
+<p className="mt-4 text-gray-600">Lorem.</p>
 </div>
 </div>
 </div>
@@ -62,4 +63,4 @@ className={tab-panel p-6 transition duration-300 ${activeTab === 2 ? 'visible op
 );
 };
 
-export default Tab;
+export default Tabs;
