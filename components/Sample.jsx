596a82cce4b67fb596be7093c8d0453da6b4dd75
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-02-24 03:17:07 +0000

    Fri, Feb 24, 2023, 3:17 AM +00:00

--- components/Sample.jsx
+++ components/Sample.jsx
@@ -1,120 +1,105 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from "react";
 
-function Tab(props) {
-  const { children, isSelected, onSelect } = props;
-  const buttonRef = useRef();
-
-  useEffect(() => {
-    if (isSelected && buttonRef.current) {
-      buttonRef.current.focus();
-    }
-  }, [isSelected]);
-
+function Tab({ label, selected, onClick }) {
   return (
     <button
-      ref={buttonRef}
       role="tab"
-      aria-selected={isSelected}
-      onClick={onSelect}
-      className={`relative block h-10 px-6 tab rounded-full ${
-        isSelected
-          ? 'bg-gray-900 text-white'
-          : 'bg-gray-900/20 text-gray-800'
+      aria-selected={selected}
+      aria-controls={`panel-${label}`}
+      id={`tab-${label}`}
+      className={`relative block h-10 px-6 rounded-full ${
+        selected
+          ? "bg-white text-gray-800"
+          : "text-gray-800/50 hover:text-gray-800/70"
       }`}
+      onClick={onClick}
     >
-      {children}
+      <span>{label}</span>
     </button>
   );
 }
 
-function TabPanel(props) {
-  const { children, id, isSelected } = props;
-
+function TabPanel({ label, selected, children }) {
   return (
     <div
       role="tabpanel"
-      id={id}
-      className={`tab-panel p-6 transition duration-300 ${
-        isSelected ? 'visible opacity-100' : 'invisible opacity-0'
-      }`}
+      id={`panel-${label}`}
+      className={`${
+        selected ? "visible opacity-100" : "invisible opacity-0"
+      } tab-panel p-6 transition duration-300`}
     >
       {children}
     </div>
   );
 }
 
-function Tabs(props) {
-  const { children } = props;
+function Tabs() {
+  const tabs = useRef([]);
+  const indicator = useRef(null);
+  const panels = useRef([]);
+
   const [selectedTab, setSelectedTab] = useState(0);
-  const tabsRef = useRef();
-  const indicatorRef = useRef();
 
   useEffect(() => {
-    const tabs = tabsRef.current.children;
-    const indicator = indicatorRef.current;
-    const activeTab = tabs[selectedTab];
-    indicator.style.width = activeTab.offsetWidth + 'px';
-    indicator.style.left =
-      activeTab.offsetLeft - tabsRef.current.offsetLeft + 'px';
+    if (tabs.current.length > 0 && panels.current.length > 0) {
+      const currentTab = tabs.current[selectedTab];
+      const currentPanel = panels.current[selectedTab];
+
+      indicator.current.style.width = `${currentTab.getBoundingClientRect().width}px`;
+      indicator.current.style.left = `${currentTab.getBoundingClientRect().left - currentTab.parentElement.getBoundingClientRect().left}px`;
+
+      panels.current.forEach((panel, index) => {
+        if (index === selectedTab) {
+          panel.classList.remove("invisible", "opacity-0");
+          panel.classList.add("visible", "opacity-100");
+        } else {
+          panel.classList.add("invisible", "opacity-0");
+        }
+      });
+    }
   }, [selectedTab]);
 
-  function handleTabSelect(index) {
+  const handleTabClick = (index) => {
     setSelectedTab(index);
-  }
+  };
 
   return (
-    <div
-      role="tablist"
-      aria-label="tabs"
-      className="relative w-max mx-auto h-12 grid grid-cols-3 items-center px-[3px] rounded-full bg-gray-900/20 overflow-hidden shadow-2xl shadow-900/20 transition"
-      ref={tabsRef}
-    >
-      <div
-        className="absolute indicator h-11 my-auto top-0 bottom-0 left-0 rounded-full bg-white shadow-md"
-        ref={indicatorRef}
-      ></div>
-      {children.map((child, index) => (
-        <Tab
-          key={index}
-          isSelected={index === selectedTab}
-          onSelect={() => handleTabSelect(index)}
-        >
-          {child.props.title}
-        </Tab>
-      ))}
+    <div className="tabs">
+      <div className="tab-list" role="tablist">
+        {React.Children.map(children, (child, index) => {
+          const isSelected = index === selectedTab;
+          return React.cloneElement(child, {
+            selected: isSelected,
+            onClick: () => handleTabClick(index),
+            ref: (el) => (tabs.current[index] = el),
+          });
+        })}
+        <div
+          className="tab-indicator"
+          ref={indicator}
+          style={{
+            position: "absolute",
+            bottom: 0,
+            left: 0,
+            height: "4px",
+            backgroundColor: "white",
+            borderRadius: "999px",
+            transition: "all 0.3s cubic-bezier(0.65, 0.05, 0.36, 1)",
+          }}
+        ></div>
+      </div>
+      <div className="tab-panels">
+        {React.Children.map(children, (child, index) => {
+          const isSelected = index === selectedTab;
+          return React.cloneElement(child.props.children, {
+            selected: isSelected,
+            ref: (el) => (panels.current[index] = el),
+          });
+        })}
+      </div>
     </div>
   );
-}
 
-function App() {
-  return (
-    <div className="min-h-screen flex items-center bg-gradient-to-br from-purple-200 to-indigo-400">
-      <div className="max-w-3xl mx-auto px-8 sm:px-0">
-        <div className="sm:w-7/12 sm:mx-auto">
-          <Tabs>
-            <TabPanel title="First Tab" id="panel-1">
-              <h2 className="text-xl font-semibold text-gray-800">
-                First tab panel
-              </h2>
-              <p className="mt-4 text-gray-600">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              </p>
-            </TabPanel>
-            <TabPanel title="Second Tab" id="panel-2">
-              <h2 className="text-xl font-semibold text-gray-800">
-                Second tab panel
-              </h2>
-              <p className="mt-4 text-gray-600">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              </p>
-            </TabPanel>
-          </Tabs>
-        </div>
-
-
-</div>
-</div>
-);
 };
 
 export default Tabs;
