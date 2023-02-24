f7618c5e634e942829a0090b6ece0665a4385ec2
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-02-24 15:50:19 +0000

    Fri, Feb 24, 2023, 3:50 PM +00:00

--- components/Tab.jsx
+++ components/Tab.jsx
@@ -11,7 +11,7 @@ function TabSelector() {
 
   return (
     <div>
-      <div className="flex justify-center pt-8 space-x-4 mb-4">
+      <div className="flex justify-center pt-8 space-x-5 mb-4">
         <button
           className={`py-2 px-4 rounded-lg ${
             selectedTab === "a" ? "bg-blue-400 text-white" : "bg-gray-200 hover:bg-blue-200 transition duration-500 ease-in-out"
