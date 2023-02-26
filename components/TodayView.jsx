9f16e9983e7e108bb1a86932af6d847d3e9a7605
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-02-26 21:52:44 +0000

    Sun, Feb 26, 2023, 9:52 PM +00:00

--- components/TodayView.jsx
+++ components/TodayView.jsx
@@ -16,7 +16,7 @@ function TodayAttendance() {
   const [searchQuery, setSearchQuery] = useState("");
   const [isLoading, setIsLoading] = useState(true);
 
-  const sections = ["1A", "1B", "2A"];
+  const sections = ["1A", "1B", "1C", "1D", "2A"];
 
   useEffect(() => {
     const fetchAttendance = async () => {
