c8f01b18d87df5cc6ab0f74e7188158810db24ab
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-02-28 10:59:01 +0000

    Tue, Feb 28, 2023, 10:59 AM +00:00

--- components/Create.jsx
+++ components/Create.jsx
@@ -47,7 +47,7 @@ function StudentCollection() {
   ]);
 
   const createCollection = async () => {
-    const studentsCollectionRef = collection(db, "strands", "STEM", "1A");
+    const studentsCollectionRef = collection(db, "strands", "STEM", "1B");
     const batch = [];
 
     students.forEach((student) => {
