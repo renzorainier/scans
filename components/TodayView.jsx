dfedd7160650b8ace124d9a47b250a3b46da8236
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-02-28 09:12:49 +0000

    Tue, Feb 28, 2023, 9:12 AM +00:00

--- components/TodayView.jsx
+++ components/TodayView.jsx
@@ -235,16 +235,16 @@ function TodayAttendance() {
           </tbody>
         </table>
         {showInfo && (
-          <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  max-w-90% bg-white p-5 rounded-lg shadow-lg inline-block">
-            <p className="text-gray-700 text-center">{infoText}</p>
-            <button
-              className="block mx-auto mt-4 px-4 py-2 bg-gray-700 text-white rounded-lg hover:bg-gray-600"
-              onClick={() => setShowInfo(false)}
-            >
-              Close
-            </button>
-          </div>
-        )}
+  <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-w-90% bg-white p-5 rounded-lg shadow-lg inline-block">
+    <p className="text-center text-gray-700">{infoText}</p>
+    <button
+      className="block mx-auto mt-4 px-4 py-2 rounded-lg text-white bg-gray-700 hover:bg-gray-600"
+      onClick={() => setShowInfo(false)}>
+      Close
+    </button>
+  </div>
+)}
+
       </div>
     </div>
   );
