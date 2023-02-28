7751f2f760a528e26f7fadf8dc7dbb6b2c5fa7c2
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-02-28 10:32:48 +0000

    Tue, Feb 28, 2023, 10:32 AM +00:00

--- components/TodayView.jsx
+++ components/TodayView.jsx
@@ -124,7 +124,7 @@ function TodayAttendance() {
       >
         <div className="bg-gray-200 relative">
           <img
-            src="your-gif-url.gif"
+            src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/fe4eba150658645.630343767747a.gif"
             alt="background gif"
             className="absolute top-0 left-0 w-full h-full object-cover z-0"
           />
