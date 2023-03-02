103af4df7a7acc33656fe3851fa303b6e14c35ff
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-03-02 10:35:09 +0000

    Thu, Mar 2, 2023, 10:35 AM +00:00

--- components/Create.jsx
+++ components/Create.jsx
@@ -4,25 +4,6 @@ import { useState } from "react";
 import { collection, addDoc, doc, setDoc, deleteDocs, getDocs, bacth } from "firebase/firestore";
 import { db } from "./firebase";
 
-const deleteCollection = async (collectionPath) => {
-  const q = query(collection(db, collectionPath));
-  const querySnapshot = await getDocs(q);
-
-  const batchDelete = batch(db);
-  querySnapshot.forEach((doc) => {
-    batchDelete(deleteDoc(doc.ref));
-  });
-
-  await batchDelete.commit();
-  console.log(`Collection ${collectionPath} deleted successfully`);
-};
-
-const DeleteCollectionButton = () => {
-  const handleDeleteCollection = async () => {
-    await deleteCollection("STEM/1B");
-  };
-
-
 function StudentCollection() {
   const [students, setStudents] = useState([
     { id: "402969160037", name: "ALARCON, RHYZEA", section: "ST-1A", strand: "STEM",  present: false, },
@@ -44,15 +25,9 @@ function StudentCollection() {
     }
   };
 
-  const handleDeleteCollection = async () => {
-    const studentsCollectionRef = collection(db, "STEM");
-    await deleteCollection(studentsCollectionRef);
-  };
-
   return (
     <div>
       <button onClick={createCollection}>Create Students Collection</button>
-      <button onClick={handleDeleteCollection}>Delete Students Collection</button>
     </div>
   );
 }
