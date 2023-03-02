cd22532906071e7bb1bce7307e1b91956c603c27
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-03-02 11:53:27 +0000

    Thu, Mar 2, 2023, 11:53 AM +00:00

--- components/Create.jsx
+++ components/Create.jsx
@@ -4,10 +4,20 @@ import { db } from "./firebase";
 
 function StudentCollection() {
   const [students, setStudents] = useState([
-    { id: "01", name: "Student 1", section: "1A", strand: "STEM", present: false },
+    { id: "01", name: "ALARCON, RHYZEA", section: "ST-1A", strand: "STEM", present: false },
     { id: "02", name: "Student 2", section: "1A", strand: "STEM", present: false },
     { id: "03", name: "Student 3", section: "1A", strand: "STEM", present: false },
   ]);
+
+
+  { id: "402969160037", name: "ALARCON, RHYZEA", section: "ST-1A", strand: "STEM" },
+  { id: "109339100105", name: "BANTOG, THAMZ JAZLEY", section: "ST-1A", strand: "STEM" },
+  { id: "114235070063", name: "BUCOG, JESSIELYN", section: "ST-1A", strand: "STEM" },
+  { id: "109327100246", name: "BUAL, GABRIEL JOHN", section: "ST-1A", strand: "STEM" },
+  { id: "109351121673", name: "DAEP, CASSANDRA", section: "ST-1A", strand: "STEM" },
+  { id: "117873090005", name: "CAMIRING, LEO", section: "ST-1A", strand: "STEM" },
+  { id: "402941150090", name: "EDANG, CHESKA", section: "ST-1A", strand: "STEM" },
+
 
   const createCollection = async () => {
     const sectionDocRef = doc(db, "STEM", "1A");
