a9c9db828def59fb98b72c0d71acd631801fcc04
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-03-02 11:47:41 +0000

    Thu, Mar 2, 2023, 11:47 AM +00:00

--- components/Create.jsx
+++ components/Create.jsx
@@ -1,47 +1,39 @@
 import { useState } from "react";
-import { collection, addDoc, doc, setDoc, deleteDoc, getDocs, batch } from "firebase/firestore";
+import { collection, doc, setDoc } from "firebase/firestore";
 import { db } from "./firebase";
 
 function StudentCollection() {
   const [students, setStudents] = useState([
-    { id: "402969160037", name: "ALARCON, RHYZEA", section: "ST-1A", strand: "STEM",  present: false, },
-    { id: "114235070063", name: "BUCOG, JESSIELYN", section: "ST-1A", strand: "STEM" },
-    { id: "109327100246", name: "BUAL, GABRIEL JOHN", section: "ST-1A", strand: "STEM" },
-    { id: "109351121673", name: "DAEP, CASSANDRA", section: "ST-1A", strand: "STEM" },
-    { id: "117873090005", name: "CAMIRING, LEO", section: "ST-1A", strand: "STEM" },
+    { id: "01", name: "Student 1", section: "ST-1A", strand: "STEM",  present: false, },
+    { id: "02", name: "Student 2", section: "ST-1A", strand: "STEM",  present: false, },
+    { id: "03", name: "Student 3", section: "ST-1A", strand: "STEM",  present: false, },
   ]);
 
   const createCollection = async () => {
-    const studentsCollectionRef = collection(db, "STEM", "1B");
-    const batchOps = batch();
+    const studentsCollectionRef = collection(db, "sections");
+    const sectionDocRef = doc(studentsCollectionRef, "ST-1A");
+    const sectionData = {};
 
     students.forEach((student) => {
-      const studentRef = doc(studentsCollectionRef, student.id);
-      batchOps.set(studentRef, { strand: student.strand, name: student.name, section: student.section});
+      const { id, name, section, strand, present } = student;
+      sectionData[`${id}name`] = name;
+      sectionData[`${id}lastScan`] = "";
+      sectionData[`${id}present`] = present;
+      sectionData["strand"] = strand;
+      sectionData["section"] = section;
     });
+
     try {
-      await batchOps.commit();
+      await setDoc(sectionDocRef, sectionData);
       console.log("Students collection created successfully");
     } catch (e) {
       console.error("Error creating students collection: ", e);
     }
   };
 
-  const deleteDocument = async () => {
-    const studentRef = doc(db, "STEM", "1B");
-    try {
-      await deleteDoc(studentRef);
-      console.log("Document deleted successfully");
-    } catch (e) {
-      console.error("Error deleting document: ", e);
-    }
-  };
-
-
   return (
     <div>
       <button onClick={createCollection}>Create Students Collection</button>
-      <button onClick={deleteDocument}>Delete Document 1B</button>
     </div>
   );
 }
