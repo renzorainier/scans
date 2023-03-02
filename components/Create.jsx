08f566f92b59106215e1446caea1f632103008e4
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-03-02 12:06:40 +0000

    Thu, Mar 2, 2023, 12:06 PM +00:00

--- components/Create.jsx
+++ components/Create.jsx
@@ -5,18 +5,18 @@ import { db } from "./firebase";
 function StudentCollection() {
   const [students, setStudents] = useState([
     { id: "01", lrn: "402969160037", name: "ALARCON, RHYZEA", section: "ST-1A", strand: "STEM", present: false },
-    { id: "02", name: "Student 2", section: "1A", strand: "STEM", present: false },
-    { id: "03", name: "Student 3", section: "1A", strand: "STEM", present: false },
+    { id: "02", lrn: "109339100105", name: "BANTOG, THAMZ JAZLEY", section: "1A", strand: "STEM", present: false },
+    { id: "03", lrn: "114235070063", name: "BUCOG, JESSIELYN", section: "1A", strand: "STEM", present: false },
   ]);
 
 
-  { id: "402969160037", name: "ALARCON, RHYZEA", section: "ST-1A", strand: "STEM" },
-  { id: "109339100105", name: "BANTOG, THAMZ JAZLEY", section: "ST-1A", strand: "STEM" },
-  { id: "114235070063", name: "BUCOG, JESSIELYN", section: "ST-1A", strand: "STEM" },
-  { id: "109327100246", name: "BUAL, GABRIEL JOHN", section: "ST-1A", strand: "STEM" },
-  { id: "109351121673", name: "DAEP, CASSANDRA", section: "ST-1A", strand: "STEM" },
-  { id: "117873090005", name: "CAMIRING, LEO", section: "ST-1A", strand: "STEM" },
-  { id: "402941150090", name: "EDANG, CHESKA", section: "ST-1A", strand: "STEM" },
+  // { id: "402969160037", name: "ALARCON, RHYZEA", section: "ST-1A", strand: "STEM" },
+  // { id: "109339100105", name: "BANTOG, THAMZ JAZLEY", section: "ST-1A", strand: "STEM" },
+  // { id: "114235070063", name: "BUCOG, JESSIELYN", section: "ST-1A", strand: "STEM" },
+  // { id: "109327100246", name: "BUAL, GABRIEL JOHN", section: "ST-1A", strand: "STEM" },
+  // { id: "109351121673", name: "DAEP, CASSANDRA", section: "ST-1A", strand: "STEM" },
+  // { id: "117873090005", name: "CAMIRING, LEO", section: "ST-1A", strand: "STEM" },
+  // { id: "402941150090", name: "EDANG, CHESKA", section: "ST-1A", strand: "STEM" },
 
 
   const createCollection = async () => {
@@ -28,6 +28,9 @@ function StudentCollection() {
       sectionData[`${id}name`] = name;
       sectionData[`${id}lastScan`] = "";
       sectionData[`${id}present`] = present;
+      sectionData[`${id}lrn`] = lrn;
+      sectionData[`${id}status`] = "";
+      sectionData[`${id}dif`] = "";
       sectionData["strand"] = strand;
       sectionData["section"] = section;
     });
