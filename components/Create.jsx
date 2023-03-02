d229524ed7a2d0146ad833bb3d79f6bb51c1e914
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-03-02 12:08:24 +0000

    Thu, Mar 2, 2023, 12:08 PM +00:00

--- components/Create.jsx
+++ components/Create.jsx
@@ -9,16 +9,6 @@ function StudentCollection() {
     { id: "03", lrn: "114235070063", name: "BUCOG, JESSIELYN", section: "1A", strand: "STEM", present: false },
   ]);
 
-
-  // { id: "402969160037", name: "ALARCON, RHYZEA", section: "ST-1A", strand: "STEM" },
-  // { id: "109339100105", name: "BANTOG, THAMZ JAZLEY", section: "ST-1A", strand: "STEM" },
-  // { id: "114235070063", name: "BUCOG, JESSIELYN", section: "ST-1A", strand: "STEM" },
-  // { id: "109327100246", name: "BUAL, GABRIEL JOHN", section: "ST-1A", strand: "STEM" },
-  // { id: "109351121673", name: "DAEP, CASSANDRA", section: "ST-1A", strand: "STEM" },
-  // { id: "117873090005", name: "CAMIRING, LEO", section: "ST-1A", strand: "STEM" },
-  // { id: "402941150090", name: "EDANG, CHESKA", section: "ST-1A", strand: "STEM" },
-
-
   const createCollection = async () => {
     const sectionDocRef = doc(db, "STEM", "1A");
     const sectionData = {};
