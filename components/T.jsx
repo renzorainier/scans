0e79e05cc1a11e5ab03f2e0bee1b2e01dcd9994e
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-03-03 14:53:33 +0000

    Fri, Mar 3, 2023, 2:53 PM +00:00

--- components/T.jsx
+++ components/T.jsx
@@ -1,6 +1,6 @@
 import { collection, getDocs } from "firebase/firestore";
 import { useEffect, useState } from "react";
-import { db } from "./firebase";
+import { db } from "./firebase.js";
 
 function useAttendanceData() {
   const [attendanceData, setAttendanceData] = useState({});
