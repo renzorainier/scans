8f685e9a6cbed63e45a126d4f4546b15b004bb04
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-03-03 08:00:54 +0000

    Fri, Mar 3, 2023, 8:00 AM +00:00

--- components/Scan.jsx
+++ components/Scan.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import { QrReader } from "react-qr-reader";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { db } from "./firebase.js";
