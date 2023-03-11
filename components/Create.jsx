54dd396879bfd33e2edfc779771299e09c71dea1
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-03-11 14:52:04 +0000

    Sat, Mar 11, 2023, 2:52 PM +00:00

--- components/Create.jsx
+++ components/Create.jsx
@@ -4,10 +4,13 @@ import { db } from "./firebase";
 
 function StudentCollection() {
   const [students, setStudents] = useState([
-    { id: "01", lrn: "109329100333", name: "MALLARI, JHAZREAL", section: "1C", strand: "STEM", present: false },
-    { id: "02", lrn: "109350100604", name: "NALZARO, CHRISTIAN", section: "1C", strand: "STEM", present: false },
-    { id: "03", lrn: "109311100426", name: "VALLECERA, ROEL", section: "1C", strand: "STEM", present: false },
-    { id: "04", lrn: "109336100225", name: "INAS, JUSTIN", section: "1C", strand: "STEM", present: false },
+
+
+  { id: "109329100329", name: "MALICDEM, CARLO", section: "ST-1B", strand: "STEM" },
+  { id: "109351121643", name: "PUREZA, XEDRICK", section: "ST-1B", strand: "STEM" },
+  { id: "109311100219", name: "ILAO, GIAN RUSSEL", section: "ST-1B", strand: "STEM" },
+  { id: "425707150124", name: "REGIS, YESTIN GABRIEL", section: "ST-1B", strand: "STEM" },
+
 
   ]);
 
@@ -23,7 +26,7 @@ function StudentCollection() {
   // { id: "03", lrn: "114235070063", name: "BUCOG, JESSIELYN", section: "1A", strand: "STEM", present: false },
 
   const createCollection = async () => {
-    const sectionDocRef = doc(db, "STEM", "1C");
+    const sectionDocRef = doc(db, "STEM", "1B");
     const sectionData = {};
 
     students.forEach((student) => {
