b81efb2ab9e5d7d2716b3fea132c9104b7d1e777
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-03-14 15:29:49 +0000

    Tue, Mar 14, 2023, 3:29 PM +00:00

--- components/Chart.jsx
+++ components/Chart.jsx
@@ -3,6 +3,7 @@ import Chart from 'chart.js/auto';
 
 const LineGraph = () => {
   const chartRef = useRef();
+  const chartInstanceRef = useRef(null);
 
   useEffect(() => {
     const chartCtx = chartRef.current.getContext('2d');
@@ -20,7 +21,12 @@ const LineGraph = () => {
       ],
     };
 
-    new Chart(chartCtx, {
+    if (chartInstanceRef.current) {
+      // Destroy previous chart if it exists
+      chartInstanceRef.current.destroy();
+    }
+
+    chartInstanceRef.current = new Chart(chartCtx, {
       type: 'line',
       data: mockData,
       options: {
@@ -33,6 +39,13 @@ const LineGraph = () => {
         },
       },
     });
+
+    // Clean up function to destroy chart when component unmounts
+    return () => {
+      if (chartInstanceRef.current) {
+        chartInstanceRef.current.destroy();
+      }
+    };
   }, []);
 
   return (
