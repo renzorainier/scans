15de2292e53e92ecbac7050db6373068fb55652f
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-03-15 11:24:36 +0000

    Wed, Mar 15, 2023, 11:24 AM +00:00

--- components/Chart.jsx
+++ components/Chart.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Chart from 'chart.js/auto';
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "./firebase.js";
