c13737beec3d129776ea073a70ba2479eac5e70f
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-03-17 12:04:24 +0000

    Fri, Mar 17, 2023, 12:04 PM +00:00

--- components/Chart.jsx
+++ components/Chart.jsx
@@ -54,8 +54,8 @@ function useAttendanceData() {
     };
 
     const minuteData = {};
-    const startHour = 19; // 6pm
-    const endHour = 20; // 12pm
+    const startHour = 17; // 6pm
+    const endHour = 21; // 12pm
     for (let hour = startHour; hour < endHour; hour++) {
       for (let minute = 0; minute < 60; minute += 1) {
         chartData.labels.push(`${hour.toString().padStart(2, "0")}:${minute.toString().padStart(2, "0")}`);
