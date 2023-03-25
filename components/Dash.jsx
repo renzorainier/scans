c0c0aa35770826c35878ddc8b018d0e87f8b2d59
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-03-25 13:08:53 +0000

    Sat, Mar 25, 2023, 1:08 PM +00:00

--- components/Dash.jsx
+++ components/Dash.jsx
@@ -42,8 +42,7 @@ const Greeting = () => {
   return (
     <div className="flex justify-center pt-1 items-center">
       <div
-        className="w-full text-gray-700 bg-white p-5  rounded-lg shadow-lg mx-auto "
-        style={{ maxWidth: "90%" }}
+        className="w-full text-gray-700 bg-white p-5  ml-5 mr-5 rounded-lg shadow-lg mx-auto "
       >
         <h2 className="text-2xl font-bold mb-2">{greeting} Scasians! </h2>
         <p className="text-gray-600 mb-2 text-sm font-bold">Today is {dateString}</p>
