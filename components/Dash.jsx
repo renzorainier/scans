43c9978e96d881cb1364d4447cbbe4f1c57462ab
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-03-25 07:22:13 +0000

    Sat, Mar 25, 2023, 7:22 AM +00:00

--- components/Dash.jsx
+++ components/Dash.jsx
@@ -11,20 +11,19 @@ const Greeting = () => {
     return () => clearInterval(timerID);
   }, []);
 
-  const qrCodeReminder = () => {
-    return "Don't forget to scan your QR code today!";
-  };
+
 
   const timeString = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
 
   return (
+    <div>
       <div className="items-center w-full text-gray-700 bg-white p-5 text-sm rounded-lg shadow-lg mx-auto">
         <h2 className="text-2xl font-bold mb-2">Good Morning Scasians!</h2>
         <p className="text-gray-600 mb-2">
           Today is {date.toLocaleDateString()}, {timeString}
         </p>
-        <p className="text-red-500 font-bold">{qrCodeReminder()}</p>
       </div>
+    </div>
   );
 };
 
