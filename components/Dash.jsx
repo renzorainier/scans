a93b3a88fc82ccc90c40413086d5521006ade590
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-03-25 08:30:02 +0000

    Sat, Mar 25, 2023, 8:30 AM +00:00

--- components/Dash.jsx
+++ components/Dash.jsx
@@ -7,50 +7,16 @@ const Greeting = () => {
   useEffect(() => {
     const morningGreetings = [
       "Good morning",
-      "Top of the morning",
-      "Rise and shine",
-      "Good day to you",
-      "Hello sunshine",
-      "Wakey wakey, eggs and bakey",
-      "Hope you slept well",
-      "Lets get this day started",
-      "Time to be awesome",
-      "Its a great day to learn",
-      "Good day, beautiful",
-      "Wishing you a lovely day ahead",
-      "May your day be filled with joy and positivity",
-      "Sending positive vibes your way",
-      "Hope you have a productive day",
-      "Lets make today count",
-      "Heres to a great day ahead",
-      "Seize the day and make it yours",
-      "Good morning, superstar!",
-      "Make today your best day yet"
+
     ];
 
     const afternoonGreetings = [
       "Good afternoon",
-      "Hello there",
-      "Howdy",
-      "Hey hey",
-      "How's your day going?",
-      "Keep up the good work",
-      "Stay strong, finish strong",
-      "We got this",
-      "Let's finish the day strong, keep it up"
+
     ];
 
     const eveningGreetings = [
       "Good evening",
-      "Greetings",
-      "Salutations",
-      "Well hello there",
-      "Time to unwind",
-      "Hope you had a great day",
-      "Rest up for tomorrow",
-      "Great job today",
-      "Let's aim to do even better tomorrow",
-      "Keep up the good work"
     ];
 
     const hour = date.getHours();
@@ -93,3 +59,44 @@ const Greeting = () => {
 };
 
 export default Greeting;
+
+
+// "Top of the morning",
+//       "Rise and shine",
+//       "Good day to you",
+//       "Hello sunshine",
+//       "Wakey wakey, eggs and bakey",
+//       "Hope you slept well",
+//       "Lets get this day started",
+//       "Time to be awesome",
+//       "Its a great day to learn",
+//       "Good day, beautiful",
+//       "Wishing you a lovely day ahead",
+//       "May your day be filled with joy and positivity",
+//       "Sending positive vibes your way",
+//       "Hope you have a productive day",
+//       "Lets make today count",
+//       "Heres to a great day ahead",
+//       "Seize the day and make it yours",
+//       "Good morning, superstar!",
+//       "Make today your best day yet"
+
+
+// "Hello there",
+// "Howdy",
+// "Hey hey",
+// "How's your day going?",
+// "Keep up the good work",
+// "Stay strong, finish strong",
+// "We got this",
+// "Let's finish the day strong, keep it up"
+
+// "Greetings",
+// "Salutations",
+// "Well hello there",
+// "Time to unwind",
+// "Hope you had a great day",
+// "Rest up for tomorrow",
+// "Great job today",
+// "Let's aim to do even better tomorrow",
+// "Keep up the good work"
