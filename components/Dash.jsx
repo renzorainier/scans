87abc9b295dc353637f56d30fb6ab24870f74f4c
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-03-25 02:09:12 +0000

    Sat, Mar 25, 2023, 2:09 AM +00:00

--- components/Dash.jsx
+++ components/Dash.jsx
@@ -16,13 +16,13 @@ const Greeting = () => {
   };
 
   return (
-    <div className="w-full text-gray-700 bg-white p-5 text-sm rounded-lg shadow-lg mx-auto">
-      <h2 className="text-2xl font-bold mb-2">Good Morning Scasians!</h2>
-      <p className="text-gray-600 mb-2">
-        Today is {date.toLocaleDateString()}, {date.toLocaleTimeString()}
-      </p>
-      <p className="text-red-500 font-bold">{qrCodeReminder()}</p>
-    </div>
+      <div className="items-center w-full text-gray-700 bg-white p-5 text-sm rounded-lg shadow-lg mx-auto">
+        <h2 className="text-2xl font-bold mb-2">Good Morning Scasians!</h2>
+        <p className="text-gray-600 mb-2">
+          Today is {date.toLocaleDateString()}, {date.toLocaleTimeString()}
+        </p>
+        <p className="text-red-500 font-bold">{qrCodeReminder()}</p>
+      </div>
   );
 };
 
