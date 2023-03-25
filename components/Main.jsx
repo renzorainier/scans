eab19a01badf0d858011e908802cdb4705a2d583
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-03-25 09:08:41 +0000

    Sat, Mar 25, 2023, 9:08 AM +00:00

--- components/Main.jsx
+++ components/Main.jsx
@@ -25,21 +25,29 @@ const MainComponent = () => {
       // render other components as needed
       default:
         return (
-          <div className="mt-4">
-            <div>
+          <div className="flex flex-col h-screen">
+            <div className="flex-shrink-0">
               <Greeting />
             </div>
-            <div className="mt-4 ml-4 mr-4">
-              <button onClick={() => handleButtonClick("today")}>
-                Attendance Today
-              </button>
-              <button onClick={() => handleButtonClick("past")}>
-                Past Attendance
-              </button>
-
+            <div className="flex-grow flex flex-row justify-center items-center">
+              <div className="flex flex-col justify-center items-center">
+                <button
+                  className="px-6 py-2 bg-green-500 text-white font-bold rounded-full shadow-lg hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75 mb-4"
+                  onClick={() => handleButtonClick("today")}
+                >
+                  Attendance Today
+                </button>
+                <button
+                  className="px-6 py-2 bg-green-500 text-white font-bold rounded-full shadow-lg hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75"
+                  onClick={() => handleButtonClick("past")}
+                >
+                  Past Attendance
+                </button>
+              </div>
             </div>
           </div>
         );
+
     }
   };
 
