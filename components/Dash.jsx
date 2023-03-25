79d73ff6b26991d80b295769e099e0f99c03bbf5
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-03-25 14:06:29 +0000

    Sat, Mar 25, 2023, 2:06 PM +00:00

--- components/Dash.jsx
+++ components/Dash.jsx
@@ -50,7 +50,7 @@ const Greeting = () => {
     } else if (timeOfDay === "afternoon") {
       return "picures/afternoon-image.jpg";
     } else {
-      return  "https://images.unsplash.com/photo-1579403124614-197f69d8187b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80";
+      return  "https://www.pinterest.ph/pin/1900024836408354=format&fit=crop&w=764&q=80";
     }
   };
 
@@ -80,6 +80,7 @@ const Greeting = () => {
         <h3 className=" mb-2">
           If you haven't already, get your qr-code scanned now :&gt;
         </h3>
+        <img src="/pictures/night.jpg" alt="hehehe" />
       </div>
     </div>
   );
