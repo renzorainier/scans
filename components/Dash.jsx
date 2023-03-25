36bb37b591a0aaf81694f6e6931d2d33a000aabf
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-03-25 08:34:13 +0000

    Sat, Mar 25, 2023, 8:34 AM +00:00

--- components/Dash.jsx
+++ components/Dash.jsx
@@ -49,6 +49,7 @@ const Greeting = () => {
         style={{ maxWidth: "90%" }}
       >
       <h2 className="text-2xl font-bold mb-2">{greeting} Scasians! </h2>
+      <h3 className="text-2xl  mb-2">{reminder} </h3>
       <p className="text-gray-600 mb-2">
         Today is {dateString}
       </p>
