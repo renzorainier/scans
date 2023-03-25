90cb8c78cdbfebd7f3a73875d31901349d5dcd11
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-03-25 07:55:58 +0000

    Sat, Mar 25, 2023, 7:55 AM +00:00

--- components/Dash.jsx
+++ components/Dash.jsx
@@ -1,61 +1,62 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const Greeting = () => {
   const [date] = useState(new Date());
   const [greeting, setGreeting] = useState('');
 
-  const morningGreetings = [
-    'Good morning',
-    'Top of the morning',
-    'Rise and shine',
-    'Good day to you',
-    'Hello sunshine',
-    'Wakey wakey, eggs and bakey',
-    'Hope you slept well',
-    'Let\'s get this day started',
-    'Time to be awesome',
-    'It\'s a great day to learn'
-  ];
+  useEffect(() => {
+    const morningGreetings = [
+      'Good morning',
+      'Top of the morning',
+      'Rise and shine',
+      'Good day to you',
+      'Hello sunshine',
+      'Wakey wakey, eggs and bakey',
+      'Hope you slept well',
+      'Let\'s get this day started',
+      'Time to be awesome',
+      'It\'s a great day to learn'
+    ];
 
-  const afternoonGreetings = [
-    'Good afternoon',
-    'Hello there',
-    'Howdy',
-    'Hey hey',
-    'How\'s your day going?',
-    'Keep up the good work',
-    'Stay strong, finish strong',
-    'We got this',
-    'Just a few more hours to go',
-    'Let\'s finish the day strong'
-  ];
+    const afternoonGreetings = [
+      'Good afternoon',
+      'Hello there',
+      'Howdy',
+      'Hey hey',
+      'How\'s your day going?',
+      'Keep up the good work',
+      'Stay strong, finish strong',
+      'We got this',
+      'Let\'s finish the day strong, keep it up'
+    ];
 
-  const eveningGreetings = [
-    'Good evening',
-    'Greetings',
-    'Salutations',
-    'Well hello there',
-    'Time to unwind',
-    'Hope you had a great day',
-    'Rest up for tomorrow',
-    'Great job today',
-    'Let\'s aim to do even better tomorrow',
-    'Keep up the good work'
-  ];
+    const eveningGreetings = [
+      'Good evening',
+      'Greetings',
+      'Salutations',
+      'Well hello there',
+      'Time to unwind',
+      'Hope you had a great day',
+      'Rest up for tomorrow',
+      'Great job today',
+      'Let\'s aim to do even better tomorrow',
+      'Keep up the good work'
+    ];
 
-  const hour = date.getHours();
-  let greetingOptions = [];
+    const hour = date.getHours();
+    let greetingOptions = [];
 
-  if (hour >= 5 && hour < 12) {
-    greetingOptions = morningGreetings;
-  } else if (hour >= 12 && hour < 18) {
-    greetingOptions = afternoonGreetings;
-  } else {
-    greetingOptions = eveningGreetings;
-  }
+    if (hour >= 5 && hour < 12) {
+      greetingOptions = morningGreetings;
+    } else if (hour >= 12 && hour < 18) {
+      greetingOptions = afternoonGreetings;
+    } else {
+      greetingOptions = eveningGreetings;
+    }
 
-  const randomIndex = Math.floor(Math.random() * greetingOptions.length);
-  setGreeting(greetingOptions[randomIndex]);
+    const randomIndex = Math.floor(Math.random() * greetingOptions.length);
+    setGreeting(greetingOptions[randomIndex]);
+  }, [date]);
 
   const dateString = date.toLocaleDateString('en-US', {
     weekday: 'long',
