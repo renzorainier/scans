c34e936e0da082e6a63b779c096455b0ff52e911
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-03-25 07:35:55 +0000

    Sat, Mar 25, 2023, 7:35 AM +00:00

--- components/Dash.jsx
+++ components/Dash.jsx
@@ -18,15 +18,24 @@ const Greeting = () => {
     day: 'numeric',
   });
 
-  const timeString = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+  const hour = date.getHours();
+  let greeting;
 
+  if (hour >= 5 && hour < 12) {
+    greeting = 'Good Morning';
+  } else if (hour >= 12 && hour < 18) {
+    greeting = 'Good Afternoon';
+  } else {
+    greeting = 'Good Evening';
+  }
+
+  const timeString = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
 
   return (
     <div className="items-center w-full text-gray-700 bg-white p-5 text-sm rounded-lg shadow-lg mx-auto">
-      <h2 className="text-2xl font-bold mb-2">Good Morning Scasians!</h2>
+      <h2 className="text-2xl font-bold mb-2">{greeting} Scasians!</h2>
       <p className="text-gray-600 mb-2">
         Today is {dateString}
-        {/* {timeString} */}
       </p>
     </div>
   );
