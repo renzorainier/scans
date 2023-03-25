01577fc3e7e3787ec92305d8fe55f895f94c3ac2
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-03-25 07:32:47 +0000

    Sat, Mar 25, 2023, 7:32 AM +00:00

--- components/Dash.jsx
+++ components/Dash.jsx
@@ -25,7 +25,8 @@ const Greeting = () => {
     <div className="items-center w-full text-gray-700 bg-white p-5 text-sm rounded-lg shadow-lg mx-auto">
       <h2 className="text-2xl font-bold mb-2">Good Morning Scasians!</h2>
       <p className="text-gray-600 mb-2">
-        Today is {dateString}, {timeString}
+        Today is {dateString}
+        {/* {timeString} */}
       </p>
     </div>
   );
