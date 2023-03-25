ba2ff63b55b13c1b6d6a9a58467027fdf2d4af38
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-03-25 07:18:59 +0000

    Sat, Mar 25, 2023, 7:18 AM +00:00

--- components/Dash.jsx
+++ components/Dash.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
 const Greeting = () => {
   const [date, setDate] = useState(new Date());
@@ -15,11 +15,13 @@ const Greeting = () => {
     return "Don't forget to scan your QR code today!";
   };
 
+  const timeString = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
   return (
       <div className="items-center w-full text-gray-700 bg-white p-5 text-sm rounded-lg shadow-lg mx-auto">
         <h2 className="text-2xl font-bold mb-2">Good Morning Scasians!</h2>
         <p className="text-gray-600 mb-2">
-          Today is {date.toLocaleDateString()}, {date.toLocaleTimeString()}
+          Today is {date.toLocaleDateString()}, {timeString}
         </p>
         <p className="text-red-500 font-bold">{qrCodeReminder()}</p>
       </div>
