3b59e25636ed21569b5f8e5ac5ab90d7ce2118f1
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-03-27 14:29:11 +0000

    Mon, Mar 27, 2023, 2:29 PM +00:00

--- components/Sample.jsx
+++ components/Sample.jsx
@@ -1,95 +1,55 @@
 import React, { useState } from 'react';
 
-const Calculator = () => {
-  const [input, setInput] = useState('');
-  const [result, setResult] = useState('');
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
 
-  const handleClick = (e) => {
-    setInput(input + e.target.value);
-  };
-
-  const handleClear = () => {
-    setInput('');
-    setResult('');
-  };
-
-  const handleEqual = () => {
-    try {
-      setResult(eval(input));
-    } catch (e) {
-      setResult('Error');
+  const handleAddTodo = () => {
+    if (inputValue.trim()) {
+      setTodos([...todos, inputValue]);
+      setInputValue('');
     }
+  };
+
+  const handleDeleteTodo = (index) => {
+    setTodos(todos.filter((_, i) => i !== index));
   };
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <div className="w-80">
-        <div className="bg-white rounded-t-lg p-4 shadow-md">
-          <div className="text-right text-gray-500">{input}</div>
-          <div className="text-right text-4xl font-bold">{result}</div>
+        <div className="bg-white rounded-lg p-4 shadow-md">
+          <input
+            className="w-full border rounded-lg py-2 px-4"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <button
+            className="bg-blue-500 text-white rounded-lg py-2 px-4 mt-2 w-full"
+            onClick={handleAddTodo}
+          >
+            Add Todo
+          </button>
         </div>
-        <div className="grid grid-cols-4 gap-1">
-          <button
-            className="bg-gray-200 rounded-lg py-3 px-4 shadow-md"
-            onClick={handleClear}
-          >
-            C
-          </button>
-          <button
-            className="bg-gray-200 rounded-lg py-3 px-4 shadow-md"
-            value="/"
-            onClick={handleClick}
-          >
-            ÷
-          </button>
-          <button
-            className="bg-gray-200 rounded-lg py-3 px-4 shadow-md"
-            value="*"
-            onClick={handleClick}
-          >
-            ×
-          </button>
-          <button
-            className="bg-gray-200 rounded-lg py-3 px-4 shadow-md"
-            value="-"
-            onClick={handleClick}
-          >
-            -
-          </button>
-          {[7, 8, 9, 4, 5, 6, 1, 2, 3].map((num) => (
-            <button
-              key={num}
-              className="bg-gray-200 rounded-lg py-3 px-4 shadow-md"
-              value={num}
-              onClick={handleClick}
+        <ul className="mt-4">
+          {todos.map((todo, index) => (
+            <li
+              key={index}
+              className="bg-white rounded-lg p-4 shadow-md flex justify-between items-center mb-2"
             >
-              {num}
-            </button>
+              <span>{todo}</span>
+              <button
+                className="bg-red-500 text-white rounded-lg py-1 px-2"
+                onClick={() => handleDeleteTodo(index)}
+              >
+                Delete
+              </button>
+            </li>
           ))}
-          <button
-            className="bg-gray-200 rounded-lg py-3 px-4 shadow-md col-span-2"
-            value="0"
-            onClick={handleClick}
-          >
-            0
-          </button>
-          <button
-            className="bg-gray-200 rounded-lg py-3 px-4 shadow-md"
-            value="."
-            onClick={handleClick}
-          >
-            .
-          </button>
-          <button
-            className="bg-blue-500 text-white rounded-lg py-3 px-4 shadow-md col-span-2"
-            onClick={handleEqual}
-          >
-            =
-          </button>
-        </div>
+        </ul>
       </div>
     </div>
   );
 };
 
-export default Calculator;
+export default TodoList;
