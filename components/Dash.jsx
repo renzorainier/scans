d0c8b3230421612bb05a0115de22673985f36632
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-03-27 05:39:28 +0000

    Mon, Mar 27, 2023, 5:39 AM +00:00

--- components/Dash.jsx
+++ components/Dash.jsx
@@ -72,7 +72,7 @@ const Greeting = () => {
       <h2 className="text-3xl font-bold mb-2">{greeting} Scasians! </h2>
       <p className="mb-2 font-bold">Today is {dateString}</p>
       {showReminder && (
-        <h3 className="text-xl mb-2 ">
+        <h3 className="text-sm font-bold mb-2 ">
           If you haven't already, get your qr-code scanned now :&gt;
         </h3>
       )}
