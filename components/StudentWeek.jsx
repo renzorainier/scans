77363eb6c57aac7b6c6c3f9830bedece36191583
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-03-28 09:51:39 +0000

    Tue, Mar 28, 2023, 9:51 AM +00:00

--- components/StudentWeek.jsx
+++ components/StudentWeek.jsx
@@ -3,51 +3,98 @@ import Chart from 'chart.js';
 
 function StudentChart({ student }) {
   const chartRef = useRef(null);
+  const chartInstanceRef = useRef(null);
 
   useEffect(() => {
-    const myChartRef = chartRef.current.getContext('2d');
+    const chartCtx = chartRef.current.getContext('2d');
 
-    new Chart(myChartRef, {
-      type: 'bar',
-      data: {
-        labels: ['A', 'B', 'C', 'D', 'E'],
-        datasets: [
-          {
-            label: 'Grades',
-            data: [student.A, student.B, student.C, student.D, student.E],
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-            ],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-            ],
-            borderWidth: 1,
-          },
-        ],
-      },
+    const chartData = formatChartData();
+
+    if (chartInstanceRef.current) {
+      // Destroy previous chart if it exists
+      chartInstanceRef.current.destroy();
+    }
+
+    chartInstanceRef.current = new Chart(chartCtx, {
+      type: 'line',
+      data: chartData,
       options: {
         scales: {
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: true,
-              },
+          yAxes: [{
+            gridLines: {
+              color: '#F5F5F5',
+              zeroLineColor: '#F5F5F5',
             },
-          ],
+            ticks: {
+              fontColor: '#888',
+              beginAtZero: true,
+              maxTicksLimit: 5,
+              padding: 10,
+            },
+          }],
+          xAxes: [{
+            gridLines: {
+              color: '#F5F5F5',
+              zeroLineColor: '#F5F5F5',
+            },
+            ticks: {
+              fontColor: '#888',
+              padding: 5,
+            },
+          }],
+        },
+        legend: {
+          display: false,
+        },
+        tooltips: {
+          backgroundColor: '#4FD1C5',
+          bodyFontColor: '#FFF',
+          titleFontColor: '#FFF',
+          titleMarginBottom: 10,
+          bodySpacing: 5,
+          xPadding: 10,
+          yPadding: 10,
+          mode: 'nearest',
+          intersect: 0,
         },
       },
     });
+
+    // Clean up function to destroy chart when component unmounts
+    return () => {
+      if (chartInstanceRef.current) {
+        chartInstanceRef.current.destroy();
+      }
+    };
   }, [student]);
 
-  return <canvas id={`chart-${student.studentId}`} ref={chartRef} />;
+  function formatChartData() {
+    const data = {
+      labels: ['A', 'B', 'C', 'D', 'E'],
+      datasets: [
+        {
+          label: 'Grades',
+          data: [
+            student.A,
+            student.B,
+            student.C,
+            student.D,
+            student.E,
+          ],
+          backgroundColor: 'rgba(79, 209, 197, 0.2)',
+          borderColor: 'rgba(79, 209, 197, 1)',
+          borderWidth: 2,
+        },
+      ],
+    };
+    return data;
+  }
+
+  return (
+    <div className="bg-white rounded-lg shadow-md mb-4 overflow-hidden">
+      <canvas id={`chart-${student.id}`} ref={chartRef} className="w-full h-full"></canvas>
+    </div>
+  );
 }
 
 export default StudentChart;
