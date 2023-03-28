0b87bb7773ab38565a1263cca8037a6b7ee2be6b
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-03-28 09:29:56 +0000

    Tue, Mar 28, 2023, 9:29 AM +00:00

--- components/TodayView.jsx
+++ components/TodayView.jsx
@@ -318,7 +318,7 @@ function AttendanceTable() {
                   <td className="p-2">
                     <div>
                       <button onClick={() => handleMoreClick(student)}>
-                        More
+                        
                       </button>
                     </div>
                   </td>
