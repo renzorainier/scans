bb29d9e0ef9ad7e0647f4c9443874ff647f1a39a
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-03-28 09:36:13 +0000

    Tue, Mar 28, 2023, 9:36 AM +00:00

--- components/StudentWeek.jsx
+++ components/StudentWeek.jsx
@@ -6,8 +6,8 @@ function StudentDetails({ student, onClose }) {
       <button className="absolute top-0 right-0 m-2 text-gray-500 hover:text-gray-700" onClick={() => onClose()}>
         <svg className="h-5 w-5 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M6.414 5.586a2 2 0 0 1 2.828 0L10 6.172l1.758-1.758a2 2 0 0 1 2.828 2.828L12.828 9l1.758 1.758a2 2 0 1 1-2.828 2.828L10 11.172l-1.758 1.758a2 2 0 1 1-2.828-2.828L7.172 9 5.414 7.242a2 2 0 0 1 0-2.828z"/></svg>
       </button>
-      <p className="text-lg font-bold mb-4">{student.name}</p>
-     
+      <p className="pt-4 text-lg font-bold mb-4">{student.name}</p>
+
     </div>
   );
 }
