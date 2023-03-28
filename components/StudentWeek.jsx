69c86639bac4448a26b95701412a9d430d707d87
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-03-28 12:41:56 +0000

    Tue, Mar 28, 2023, 12:41 PM +00:00

--- components/StudentWeek.jsx
+++ components/StudentWeek.jsx
@@ -91,9 +91,16 @@ function StudentChart({ student }) {
   }
 
   return (
-    <div className="bg-white rounded-lg shadow-md mb-4 overflow-hidden">
+    <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-md shadow-md p-4">
+      <button className=" text-lg font-bold absolute top-0 right-0 m-2 text-gray-500 hover:text-gray-700" onClick={() => onClose()}>
+        X
+      </button>
+      <p className="pt-4 text-lg font-bold mb-4">{student.name}</p>
+      <div className="bg-white rounded-lg shadow-md mb-4 overflow-hidden">
       <canvas id={`chart-${student.id}`} ref={chartRef} className="w-full h-full"></canvas>
     </div>
+    </div>
+
   );
 }
 
