f60f6d5b940a63626067b292dd8a6d5aaf75ffa9
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-03 03:44:26 +0000

    Mon, Apr 3, 2023, 3:44 AM +00:00

--- components/StudentWeek.jsx
+++ components/StudentWeek.jsx
@@ -146,52 +146,20 @@ function StudentChart({ student, onClose }) {
         ></canvas>
       </div>
       <div>
-          {student.As === "late" && (
-            <div className="bg-[#EC7063] h-6 w-6 rounded-sm inline-block mr-1"></div>
-          )}
-          {student.As === "ontime" && (
-            <div className="bg-[#F7DC6F]  h-6 w-6 rounded-sm inline-block mr-1"></div>
-          )}
-          {student.As === "early" && (
-            <div className="bg-[#2ECC71]  h-6 w-6 rounded-sm inline-block mr-1"></div>
-          )}
-          {student.Bs === "late" && (
-            <div className="bg-[#EC7063] h-6 w-6 rounded-sm inline-block mr-1"></div>
-          )}
-          {student.Bs === "ontime" && (
-            <div className="bg-[#F7DC6F]  h-6 w-6 rounded-sm inline-block mr-1"></div>
-          )}
-          {student.Bs === "early" && (
-            <div className="bg-[#2ECC71]  h-6 w-6 rounded-sm inline-block mr-1"></div>
-          )}
-          {student.Cs === "late" && (
-            <div className="bg-[#EC7063] h-6 w-6 rounded-sm inline-block mr-1"></div>
-          )}
-          {student.Cs === "ontime" && (
-            <div className="bg-[#F7DC6F]  h-6 w-6 rounded-sm inline-block mr-1"></div>
-          )}
-          {student.Cs === "early" && (
-            <div className="bg-[#2ECC71]  h-6 w-6 rounded-sm inline-block mr-1"></div>
-          )}
-          {student.Ds === "late" && (
-            <div className="bg-[#EC7063] h-6 w-6 rounded-sm inline-block mr-1"></div>
-          )}
-          {student.Ds === "ontime" && (
-            <div className="bg-[#F7DC6F]  h-6 w-6 rounded-sm inline-block mr-1"></div>
-          )}
-          {student.Ds === "early" && (
-            <div className="bg-[#2ECC71]  h-6 w-6 rounded-sm inline-block mr-1"></div>
-          )}
-          {student.Es === "late" && (
-            <div className="bg-[#EC7063] h-6 w-6 rounded-sm inline-block mr-1"></div>
-          )}
-          {student.Es === "ontime" && (
-            <div className="bg-[#F7DC6F]  h-6 w-6 rounded-sm inline-block mr-1"></div>
-          )}
-          {student.Es === "early" && (
-            <div className="bg-[#2ECC71]  h-6 w-6 rounded-sm inline-block mr-1"></div>
-          )}
-        </div>
+  {["As", "Bs", "Cs", "Ds", "Es"].map((grade) => (
+    <div
+      className={`bg-${
+        student[grade.toLowerCase()] === "late"
+          ? "#EC7063"
+          : student[grade.toLowerCase()] === "ontime"
+          ? "#F7DC6F"
+          : "#2ECC71"
+      } h-6 w-6 rounded-sm inline-block mr-1`}
+      key={grade}
+    ></div>
+  ))}
+</div>
+
     </div>
   );
 }
