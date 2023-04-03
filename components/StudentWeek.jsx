fd8068e27ff75cf17fc0656fdf808a4ab1128c98
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-03 02:49:07 +0000

    Mon, Apr 3, 2023, 2:49 AM +00:00

--- components/StudentWeek.jsx
+++ components/StudentWeek.jsx
@@ -135,7 +135,7 @@ function StudentChart({ student, onClose }) {
         className=" text-lg font-bold absolute top-0 right-0 m-2 text-gray-500 hover:text-gray-700"
         onClick={() => onClose()}
       >
-        X
+      
       </button>
       <p className="pt-4 text-lg font-bold mb-4">{student.name}</p>
       <div className="bg-white rounded-lg shadow-md mb-4 overflow-hidden">
