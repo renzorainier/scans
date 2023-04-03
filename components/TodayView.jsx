6ae59d3868e043a4295c8121363804f4cf55b8c0
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-03 03:07:57 +0000

    Mon, Apr 3, 2023, 3:07 AM +00:00

--- components/TodayView.jsx
+++ components/TodayView.jsx
@@ -179,6 +179,8 @@ function AttendanceTable() {
           <LineGraph data={attendanceData} />
         </div>
 
+
+
         <div className="flex justify-between items-center mb-4">
           <div id="search" className="flex items-center w-full">
             <input
@@ -322,6 +324,20 @@ function AttendanceTable() {
               )}
             </tbody>
           </table>
+          <div>
+          {["As", "Bs", "Cs", "Ds", "Es",].map((prop) => (
+            <div
+              key={prop}
+              className={`bg-${
+                {
+                  late: "#EC7063",
+                  ontime: "#F7DC6F",
+                  early: "#2ECC71",
+                }[student[prop]]
+              } h-6 w-16 rounded-sm inline-block mr-1`}
+            ></div>
+          ))}
+        </div>
           {showInfo && (
             <div
               id="show"
