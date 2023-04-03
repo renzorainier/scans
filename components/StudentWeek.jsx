6c41d2f6cb52377bb987d0727258c2703a6e9148
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-03 03:59:26 +0000

    Mon, Apr 3, 2023, 3:59 AM +00:00

--- components/StudentWeek.jsx
+++ components/StudentWeek.jsx
@@ -131,21 +131,21 @@ function StudentChart({ student, onClose }) {
 
   return (
     <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-md shadow-md p-4 w-3/4 h-1/2">
-      <button
-        className=" text-lg font-bold absolute top-0 right-0 m-2 text-gray-500 hover:text-gray-700"
-        onClick={() => onClose()}
-      >
-        X
-      </button>
-      <p className="pt-4 text-lg font-bold mb-4">{student.name}</p>
-      <div className="bg-white rounded-lg shadow-md mb-4 overflow-hidden">
-        <canvas
-          id={`chart-${student.id}`}
-          ref={chartRef}
-          className="w-full h-full"
-        ></canvas>
-      </div>
-      <div>
+    <button
+      className="text-lg font-bold absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+      onClick={() => onClose()}
+    >
+      X
+    </button>
+    <p className="pt-4 text-lg font-bold mb-4">{student.name}</p>
+    <div className="bg-white rounded-lg shadow-md mb-4 overflow-hidden">
+      <canvas
+        id={`chart-${student.id}`}
+        ref={chartRef}
+        className="w-full h-full"
+      ></canvas>
+    </div>
+    <div>
         {student.As === "late" && (
           <div className="bg-[#EC7063] h-6 w-6 rounded-sm inline-block mr-1"></div>
         )}
@@ -192,8 +192,58 @@ function StudentChart({ student, onClose }) {
           <div className="bg-[#2ECC71]  h-6 w-6 rounded-sm inline-block mr-1"></div>
         )}
       </div>
-    </div>
+  </div>
+
   );
 }
 
 export default StudentChart;
+
+
+// {/* <div>
+//         {student.As === "late" && (
+//           <div className="bg-[#EC7063] h-6 w-6 rounded-sm inline-block mr-1"></div>
+//         )}
+//         {student.As === "ontime" && (
+//           <div className="bg-[#F7DC6F]  h-6 w-6 rounded-sm inline-block mr-1"></div>
+//         )}
+//         {student.As === "early" && (
+//           <div className="bg-[#2ECC71]  h-6 w-6 rounded-sm inline-block mr-1"></div>
+//         )}
+//         {student.Bs === "late" && (
+//           <div className="bg-[#EC7063] h-6 w-6 rounded-sm inline-block mr-1"></div>
+//         )}
+//         {student.Bs === "ontime" && (
+//           <div className="bg-[#F7DC6F]  h-6 w-6 rounded-sm inline-block mr-1"></div>
+//         )}
+//         {student.Bs === "early" && (
+//           <div className="bg-[#2ECC71]  h-6 w-6 rounded-sm inline-block mr-1"></div>
+//         )}
+//         {student.Cs === "late" && (
+//           <div className="bg-[#EC7063] h-6 w-6 rounded-sm inline-block mr-1"></div>
+//         )}
+//         {student.Cs === "ontime" && (
+//           <div className="bg-[#F7DC6F]  h-6 w-6 rounded-sm inline-block mr-1"></div>
+//         )}
+//         {student.Cs === "early" && (
+//           <div className="bg-[#2ECC71]  h-6 w-6 rounded-sm inline-block mr-1"></div>
+//         )}
+//         {student.Ds === "late" && (
+//           <div className="bg-[#EC7063] h-6 w-6 rounded-sm inline-block mr-1"></div>
+//         )}
+//         {student.Ds === "ontime" && (
+//           <div className="bg-[#F7DC6F]  h-6 w-6 rounded-sm inline-block mr-1"></div>
+//         )}
+//         {student.Ds === "early" && (
+//           <div className="bg-[#2ECC71]  h-6 w-6 rounded-sm inline-block mr-1"></div>
+//         )}
+//         {student.Es === "late" && (
+//           <div className="bg-[#EC7063] h-6 w-6 rounded-sm inline-block mr-1"></div>
+//         )}
+//         {student.Es === "ontime" && (
+//           <div className="bg-[#F7DC6F]  h-6 w-6 rounded-sm inline-block mr-1"></div>
+//         )}
+//         {student.Es === "early" && (
+//           <div className="bg-[#2ECC71]  h-6 w-6 rounded-sm inline-block mr-1"></div>
+//         )}
+//       </div> */}
