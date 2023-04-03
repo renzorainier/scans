4b166c79b3d2462c2b2fd42e4b78b1748845f79a
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-03 03:29:42 +0000

    Mon, Apr 3, 2023, 3:29 AM +00:00

--- components/StudentWeek.jsx
+++ components/StudentWeek.jsx
@@ -130,7 +130,7 @@ function StudentChart({ student, onClose }) {
   }
 
   return (
-    <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-md shadow-md p-4">
+    <div id="view" className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white  rounded-md shadow-md p-4">
       <button
         className=" text-lg font-bold absolute top-0 right-0 m-2 text-gray-500 hover:text-gray-700"
         onClick={() => onClose()}
