a91a9af8d5522c4c0b3391d56e3d24f0d21c27e4
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-04 15:20:51 +0000

    Tue, Apr 4, 2023, 3:20 PM +00:00

--- components/StudentWeek.jsx
+++ components/StudentWeek.jsx
@@ -1,6 +1,8 @@
-import React, { useRef, useEffect } from "react";
+import React from "react";
 
 function StudentChart({ student, onClose }) {
+  const grades = ["As", "Bs", "Cs", "Ds", "Es"];
+
   return (
     <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-md shadow-md p-4 w-3/4 h-1/2">
       <button
@@ -14,68 +16,29 @@ function StudentChart({ student, onClose }) {
         {student.strand}-{student.section}
       </p>
       <div>
-        <div>
-          {student.As === "late" && (
-            <div className="bg-[#EC7063] h-6 w-6 rounded-sm inline-block m-3"></div>
-          )}
-          {student.As === "ontime" && (
-            <div className="bg-[#F7DC6F]  h-6 w-6 rounded-sm inline-block m-3"></div>
-          )}
-          {student.As === "early" && (
-            <div className="bg-[#2ECC71]  h-6 w-6 rounded-sm inline-block m-3"></div>
-          )}
-          {student.As === "" && (
-            <div className="bg-[#BDCDD6]  h-6 w-6 rounded-sm inline-block m-3"></div>
-          )}
-          {student.Bs === "late" && (
-            <div className="bg-[#EC7063] h-6 w-6 rounded-sm inline-block m-3"></div>
-          )}
-          {student.Bs === "ontime" && (
-            <div className="bg-[#F7DC6F]  h-6 w-6 rounded-sm inline-block m-3"></div>
-          )}
-          {student.Bs === "early" && (
-            <div className="bg-[#2ECC71]  h-6 w-6 rounded-sm inline-block m-3"></div>
-          )}
-          {student.Bs === "" && (
-            <div className="bg-[#BDCDD6]  h-6 w-6 rounded-sm inline-block m-3"></div>
-          )}
-          {student.Cs === "late" && (
-            <div className="bg-[#EC7063] h-6 w-6 rounded-sm inline-block m-3"></div>
-          )}
-          {student.Cs === "ontime" && (
-            <div className="bg-[#F7DC6F]  h-6 w-6 rounded-sm inline-block m-3"></div>
-          )}
-          {student.Cs === "early" && (
-            <div className="bg-[#2ECC71]  h-6 w-6 rounded-sm inline-block m-3"></div>
-          )}
-          {student.Cs === "" && (
-            <div className="bg-[#BDCDD6]  h-6 w-6 rounded-sm inline-block m-3"></div>
-          )}
-          {student.Ds === "late" && (
-            <div className="bg-[#EC7063] h-6 w-6 rounded-sm inline-block m-3"></div>
-          )}
-          {student.Ds === "ontime" && (
-            <div className="bg-[#F7DC6F]  h-6 w-6 rounded-sm inline-block m-3"></div>
-          )}
-          {student.Ds === "early" && (
-            <div className="bg-[#2ECC71]  h-6 w-6 rounded-sm inline-block m-3"></div>
-          )}
-          {student.Ds === "" && (
-            <div className="bg-[#BDCDD6]  h-6 w-6 rounded-sm inline-block m-3"></div>
-          )}
-          {student.Es === "late" && (
-            <div className="bg-[#EC7063] h-6 w-6 rounded-sm inline-block m-3"></div>
-          )}
-          {student.Es === "ontime" && (
-            <div className="bg-[#F7DC6F]  h-6 w-6 rounded-sm inline-block m-3"></div>
-          )}
-          {student.Es === "early" && (
-            <div className="bg-[#2ECC71]  h-6 w-6 rounded-sm inline-block m-3"></div>
-          )}
-          {student.Es === "" && (
-            <div className="bg-[#BDCDD6]  h-6 w-6 rounded-sm inline-block m-3"></div>
-          )}
-        </div>
+        {grades.map((grade, index) => {
+          let color = "";
+          switch (student[grade]) {
+            case "late":
+              color = "bg-[#EC7063]";
+              break;
+            case "ontime":
+              color = "bg-[#F7DC6F]";
+              break;
+            case "early":
+              color = "bg-[#2ECC71]";
+              break;
+            default:
+              color = "bg-[#BDCDD6]";
+              break;
+          }
+          return (
+            <div
+              key={index}
+              className={`${color} h-6 w-6 rounded-sm inline-block m-3`}
+            ></div>
+          );
+        })}
       </div>
     </div>
   );
