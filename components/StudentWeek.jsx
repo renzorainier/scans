5d73cac64eaceb99144345701731722ff31a9b09
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-04 15:55:35 +0000

    Tue, Apr 4, 2023, 3:55 PM +00:00

--- components/StudentWeek.jsx
+++ components/StudentWeek.jsx
@@ -2,7 +2,7 @@ function StudentChart({ student, onClose }) {
   const grades = ["Mon", "Tue", "Wed", "Thu", "Fri"];
 
   return (
-    <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-md shadow-md p-4 ">
+    <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-md shadow-md p-4 w-4/5 h-3/2">
       <button
         className="text-lg font-bold absolute top-2 right-2 text-gray-500 hover:text-gray-700"
         onClick={() => onClose()}
