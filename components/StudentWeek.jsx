5acf373f4a5cb37f7c1acad18b77405d76a72fe4
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-05 08:04:13 +0000

    Wed, Apr 5, 2023, 8:04 AM +00:00

--- components/StudentWeek.jsx
+++ components/StudentWeek.jsx
@@ -3,96 +3,98 @@ function StudentChart({ student, onClose }) {
   const times = ["A", "B", "C", "D", "E"];
 
   return (
-    <div className="fixed z-50 top-0 left-0 w-screen h-screen bg-red-500 opacity-75"></div>
-    <div className="fixed z-50 top-1/2 left-1/2  transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-md shadow-xl p-8 w-4/5 max-w-md">
-      <button
-        className="absolute top-3 right-3 text-gray-500 hover:text-gray-700"
-        onClick={onClose}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
+    <div>
+      <div className="fixed z-50 top-0 left-0 w-screen h-screen bg-red-500 opacity-75"></div>
+      <div className="fixed z-50 top-1/2 left-1/2  transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-md shadow-xl p-8 w-4/5 max-w-md">
+        <button
+          className="absolute top-3 right-3 text-gray-500 hover:text-gray-700"
+          onClick={onClose}
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M6 18L18 6M6 6l12 12"
-          />
-        </svg>
-      </button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
 
-      <div className="p-4">
-        <p className="text-xl font-bold">{student.name}</p>
-        <p className="text-gray-500 text-base">
-          {student.strand}-{student.section}
-        </p>
-      </div>
+        <div className="p-4">
+          <p className="text-xl font-bold">{student.name}</p>
+          <p className="text-gray-500 text-base">
+            {student.strand}-{student.section}
+          </p>
+        </div>
 
-      <div className=" flex justify-between mt-8">
-        {grades.map((grade, index) => {
-          let gradeValue;
-          switch (grade) {
-            case "Mon":
-              gradeValue = "As";
-              break;
-            case "Tue":
-              gradeValue = "Bs";
-              break;
-            case "Wed":
-              gradeValue = "Cs";
-              break;
-            case "Thu":
-              gradeValue = "Ds";
-              break;
-            case "Fri":
-              gradeValue = "Es";
-              break;
-            default:
-              break;
-          }
-          let color = "";
-          switch (student[gradeValue]) {
-            case "late":
-              color = "bg-red-500";
-              break;
-            case "ontime":
-              color = "bg-yellow-400";
-              break;
-            case "early":
-              color = "bg-green-500";
-              break;
-            default:
-              color = "bg-gray-400";
-              break;
-          }
-          const timeValue = student[times[index]];
-          return (
-            <div key={index} className="flex flex-col items-center">
-              <p className="text-xs font-semibold text-gray-500">{grade}</p>
-              <div className={`${color} h-6 w-6 rounded-full my-4`}></div>
-              {timeValue && times.includes(times[index]) ? (
-                <p className="text-xs font-semibold text-gray-500">
-                  {new Date(timeValue.seconds * 1000).toLocaleTimeString([], {
-                    hour: "numeric",
-                    minute: "2-digit",
-                    hour12: false,
-                  })}
-                </p>
-              ) : (
-                <p className="text-xs font-semibold text-gray-500">
-                  no record
-                </p>
-              )}
-            </div>
-          );
-        })}
+        <div className=" flex justify-between mt-8">
+          {grades.map((grade, index) => {
+            let gradeValue;
+            switch (grade) {
+              case "Mon":
+                gradeValue = "As";
+                break;
+              case "Tue":
+                gradeValue = "Bs";
+                break;
+              case "Wed":
+                gradeValue = "Cs";
+                break;
+              case "Thu":
+                gradeValue = "Ds";
+                break;
+              case "Fri":
+                gradeValue = "Es";
+                break;
+              default:
+                break;
+            }
+            let color = "";
+            switch (student[gradeValue]) {
+              case "late":
+                color = "bg-red-500";
+                break;
+              case "ontime":
+                color = "bg-yellow-400";
+                break;
+              case "early":
+                color = "bg-green-500";
+                break;
+              default:
+                color = "bg-gray-400";
+                break;
+            }
+            const timeValue = student[times[index]];
+            return (
+              <div key={index} className="flex flex-col items-center">
+                <p className="text-xs font-semibold text-gray-500">{grade}</p>
+                <div className={`${color} h-6 w-6 rounded-full my-4`}></div>
+                {timeValue && times.includes(times[index]) ? (
+                  <p className="text-xs font-semibold text-gray-500">
+                    {new Date(timeValue.seconds * 1000).toLocaleTimeString([], {
+                      hour: "numeric",
+                      minute: "2-digit",
+                      hour12: false,
+                    })}
+                  </p>
+                ) : (
+                  <p className="text-xs font-semibold text-gray-500">
+                    no record
+                  </p>
+                )}
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
-  }
+}
 
 export default StudentChart;
