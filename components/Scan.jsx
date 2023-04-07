c9d6ff02d4b10c359e033ffaf1d51ec136798e6c
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-07 16:13:41 +0000

    Fri, Apr 7, 2023, 4:13 PM +00:00

--- components/Scan.jsx
+++ components/Scan.jsx
@@ -307,21 +307,22 @@ function Scan() {
   return (
     <div>
       <QrReader
-       onResult={async (result) => {
-        if (!!result) {
-          const code = result.text;
-          if (code !== lastScanned) {
-            const decodedCode = code.split("").map(char => mappingTable[char] || "").join("");
-            setLastScanned(decodedCode);
-            handleMarkPresent(decodedCode);
+        onResult={async (result) => {
+          if (!!result) {
+            const code = result.text;
+            if (code !== lastScanned) {
+              // Process the scanned code using the mapping table
+              const decodedCode = code.split('').map(char => mappingTable[char]).join('');
+              setLastScanned(code);
+              handleMarkPresent(decodedCode);
+            }
           }
-        }
-      }}
-      constraints={{ facingMode: "environment" }}
-      style={{ width: "100%", height: "100%" }}
+        }}
+        constraints={{ facingMode: "environment" }}
+        style={{ width: "100%", height: "100%" }}
       />
       <p className="text-xl font-bold mt-6">Scan result:</p>
-      <p className="text-xl">{decodedData}</p>
+      <p className="text-xl">{data}</p>
       <h1 className="text-3xl font-semibold mt-8">Recent Logs</h1>
       <div className="bg-white rounded-lg shadow-lg mt-6 w-full max-w-md">
         <ul className="text-gray-500 divide-y divide-gray-300">
@@ -333,8 +334,7 @@ function Scan() {
         </ul>
       </div>
     </div>
-  );
-}
+  )}
 
 export default Scan;
 
