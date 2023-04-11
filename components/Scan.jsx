c437b8401b33caa3dbc1e81c9e622ee12080d1ab
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-11 14:06:56 +0000

    Tue, Apr 11, 2023, 2:06 PM +00:00

--- components/Scan.jsx
+++ components/Scan.jsx
@@ -79,7 +79,7 @@ function Scan() {
           startTime: "17:00:00",
         },
         Tuesday: {
-          startTime: "19:10:00",
+          startTime: "23:59:00",
         },
         Wednesday: {
           startTime: "08:00:00",
