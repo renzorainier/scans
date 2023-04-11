d4d1d8f1abfb3fc2f0bf77a6a87d3cfde6e434dc
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-11 08:53:06 +0000

    Tue, Apr 11, 2023, 8:53 AM +00:00

--- pages/index.js
+++ pages/index.js
@@ -124,7 +124,7 @@ export default function Home() {
       {/* <main className="container mx-auto ">{renderComponent()}</main> */}
     <Main />
     <StudentCollection />
-    {/* <BadgeWriter /> */}
+    <BadgeWriter />
     {/* <BadgeReader /> */}
     </>
   );
