1b41daec08dc37c565f2d346ba23451a3b4a706f
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-12 07:01:10 +0000

    Wed, Apr 12, 2023, 7:01 AM +00:00

--- components/Rank.jsx
+++ components/Rank.jsx
@@ -2,32 +2,38 @@ function Rank({ data, onClose }) {
 console.log(data)
 
   return (
-    <div>
-      <div className="fixed z-50 top-0 left-0 w-screen h-screen backdrop-blur-xl bg-gray/90"></div>
-      <div className="fixed z-50 top-1/2 left-1/2  transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-md shadow-xl p-8 w-4/5 h-4/5 max-w-md">
-        <button
-          className="absolute top-3 right-3 text-gray-500 hover:text-gray-700"
-          onClick={onClose}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
-        </button>
-
-        </div>
-      </div>
+<div>
+  hehe
+</div>
   );
 }
 
 export default Rank;
+
+
+
+/* <div>
+<div className="fixed z-50 top-0 left-0 w-screen h-screen backdrop-blur-xl bg-gray/90"></div>
+<div className="fixed z-50 top-1/2 left-1/2  transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-md shadow-xl p-8 w-4/5 h-4/5 max-w-md">
+  <button
+    className="absolute top-3 right-3 text-gray-500 hover:text-gray-700"
+    onClick={onClose}
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M6 18L18 6M6 6l12 12"
+      />
+    </svg>
+  </button>
+
+  </div>
+</div> */
