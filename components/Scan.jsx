649609d5f07330740136d1d19f0d14a4d3f1ecfc
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-12 03:10:49 +0000

    Wed, Apr 12, 2023, 3:10 AM +00:00

--- components/Scan.jsx
+++ components/Scan.jsx
@@ -273,6 +273,7 @@ function Scan() {
         //   studentData[`${id}badge`] = topNumber;
         // }
         await updateDoc(sectionRef, studentData);
+        await updateDoc(badgeRef, updatedData);
         console.log(
           `Student ${id} marked as present with ${attendanceStatus} status`
         );
