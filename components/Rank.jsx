8de6cafea3a91085551a2515027739b3d351e975
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-12 12:47:57 +0000

    Wed, Apr 12, 2023, 12:47 PM +00:00

--- components/Rank.jsx
+++ components/Rank.jsx
@@ -4,14 +4,37 @@ function Rank({ data, onClose }) {
   const earliestStudents = {};
   const overallEarliest = [];
 
+  // Get the day code based on the current day of the week
+  const dayOfWeek = new Date().toLocaleString("en-US", { weekday: "short" });
+  let dayCode;
+  switch (dayOfWeek) {
+    case "Mon":
+      dayCode = "A";
+      break;
+    case "Tue":
+      dayCode = "B";
+      break;
+    case "Wed":
+      dayCode = "C";
+      break;
+    case "Thu":
+      dayCode = "D";
+      break;
+    case "Fri":
+      dayCode = "E";
+      break;
+    default:
+      dayCode = "X";
+  }
+
   Object.keys(data).forEach((section) => {
     const sectionData = data[section];
     earliestStudents[section] = [];
     Object.keys(sectionData).forEach((student) => {
       const studentData = sectionData[student];
-      const lastScan = studentData["lastScan"];
-      if (lastScan) {
-        const scanTime = lastScan.seconds * 1000;
+      const dayData = studentData[dayCode];
+      if (dayData) {
+        const scanTime = dayData.seconds * 1000;
         const formattedTime = new Date(scanTime).toLocaleTimeString();
         if (
           !earliestStudents[section][9] ||
@@ -31,7 +54,6 @@ function Rank({ data, onClose }) {
             overallEarliest.push({
               name: studentData.name,
               section: studentData.section,
-
               student,
               scanTime,
               formattedTime,
@@ -47,47 +69,76 @@ function Rank({ data, onClose }) {
   return (
     <div>
       <div>
-        <div className="fixed z-50 top-0 left-0 w-screen h-screen backdrop-blur-xl bg-gray/90"></div>
-        <div className="fixed z-50 top-1/2 left-1/2  transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-md shadow-xl p-8 w-4/5 h-full max-w-md">
-          <button
-            className="absolute top-3 right-3 text-gray-500 hover:text-gray-700"
-            onClick={onClose}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </button>
-          <h2>Overall Top 10 Earliest Students</h2>
-      <ol>
-        {overallEarliest.map(({ name, student, section,   formattedTime }) => (
-          <li key={student}>{name}- {formattedTime} - {section}</li>
-        ))}
-      </ol>
+        <h2>Overall Top 10</h2>
+        <ol>
+          {overallEarliest.map(({ name, section, formattedTime }) => (
+            <li key={name}>
+              {name} - {section} - {formattedTime}
+            </li>
+          ))}
+        </ol>
+      </div>
       {Object.keys(earliestStudents).map((section) => (
         <div key={section}>
           <h2>{section}</h2>
           <ol>
             {earliestStudents[section].map(({ name, student, formattedTime }) => (
-              <li key={student}>{name} - {formattedTime} </li>
+              <li key={student}>
+                {name} - {formattedTime}
+              </li>
             ))}
           </ol>
         </div>
       ))}
-        </div>
-      </div>
     </div>
   );
 }
 
 export default Rank;
+
+
+
+
+
+{/* <div>
+<div>
+  <div className="fixed z-50 top-0 left-0 w-screen h-screen backdrop-blur-xl bg-gray/90"></div>
+  <div className="fixed z-50 top-1/2 left-1/2  transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-md shadow-xl p-8 w-4/5 h-full max-w-md">
+    <button
+      className="absolute top-3 right-3 text-gray-500 hover:text-gray-700"
+      onClick={onClose}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M6 18L18 6M6 6l12 12"
+        />
+      </svg>
+    </button>
+    <h2>Overall Top 10 Earliest Students</h2>
+<ol>
+  {overallEarliest.map(({ name, student, section,   formattedTime }) => (
+    <li key={student}>{name}- {formattedTime} - {section}</li>
+  ))}
+</ol>
+{Object.keys(earliestStudents).map((section) => (
+  <div key={section}>
+    <h2>{section}</h2>
+    <ol>
+      {earliestStudents[section].map(({ name, student, formattedTime }) => (
+        <li key={student}>{name} - {formattedTime} </li>
+      ))}
+    </ol>
+  </div>
+))}
+  </div>
+</div>
+</div> */}
