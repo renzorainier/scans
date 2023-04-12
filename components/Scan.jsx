f530e10a2d7f77f9d5ee7b99059bd573fef897b8
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-12 03:03:41 +0000

    Wed, Apr 12, 2023, 3:03 AM +00:00

--- components/Scan.jsx
+++ components/Scan.jsx
@@ -221,6 +221,9 @@ function Scan() {
                 console.log(topNumber);
                 if (topNumber !== "") {
                   studentData[`${id}badge`] = topNumber;
+                  const updatedData = {};
+                  updatedData[fieldName] = true;
+                  await updateDoc(badgeRef, updatedData);
                 }
                 nullFieldFound = true;
               }
