4f490e1bdd9c0b0a5b51a8e9bbe56ffc174c3aa1
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-12 13:53:00 +0000

    Wed, Apr 12, 2023, 1:53 PM +00:00

--- components/TodayView.jsx
+++ components/TodayView.jsx
@@ -166,20 +166,20 @@ function AttendanceTable() {
 
   return (
     <div>
-       <div
+      <div
       className="md:w-14 md:h-7 w-12 h-6 flex items-center bg-gray-300 rounded-full p-1 cursor-pointer"
       onClick={() => {
         setToggle(!toggle);
       }}
     >
 
+   {/* Switch */}
       <div
-        className =  {"bg-white md:w-6 md:h-6 h-5 w-5 rounded-full shadow-md
-         transform" +  (toggle ? null : toggleClass)}
-      ></div>
-      </div>
-      </div>
+        className =  {"bg-white md:w-6 md:h-6 h-5 w-5 rounded-full shadow-md transform" +  (toggle ? null : toggleClass)}
+      >
+</div>
 
+</div>
 
       <button onClick={() => handleToggle(true)}>Show A</button>
       <button onClick={() => handleToggle(false)}>Show B</button>
