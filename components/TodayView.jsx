4df4fc39b3640bbcf6741dc66bf903221c3cdc53
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-12 14:10:29 +0000

    Wed, Apr 12, 2023, 2:10 PM +00:00

--- components/TodayView.jsx
+++ components/TodayView.jsx
@@ -169,14 +169,14 @@ function AttendanceTable() {
       <Switch
         checked={enabled}
         onChange={setEnabled}
-        className={`${enabled ? 'bg-teal-900' : 'bg-teal-700'}
-          relative inline-flex h-[38px] w-[74px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+        className={`${enabled ? 'bg-gray-300' : 'bg-gray-400'}
+          relative inline-flex h-[38px] w-[74px] shrink-0 cursor-pointer rounded-lg border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
       >
         <span className="sr-only">Use setting</span>
         <span
           aria-hidden="true"
           className={`${enabled ? 'translate-x-9' : 'translate-x-0'}
-            pointer-events-none inline-block h-[34px] w-[34px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+            pointer-events-none inline-block h-[34px] w-[34px] transform rounded-lg bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
         />
       </Switch>
     </div>
