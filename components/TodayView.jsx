5726e739f1b66bb285cb0ef79a58b5a929ddfe0b
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-13 10:19:16 +0000

    Thu, Apr 13, 2023, 10:19 AM +00:00

--- components/TodayView.jsx
+++ components/TodayView.jsx
@@ -183,10 +183,13 @@ function AttendanceTable() {
         </div>
       </div>
 
-      <div >
+      <div>
         <div>
           {state ? (
-            <div>
+            <div
+              className="w-full rounded-lg mx-auto"
+              style={{ maxWidth: "90%" }}
+            >
               <Rank data={attendanceData} />
               <TransparentTabs data={attendanceData} />
             </div>
