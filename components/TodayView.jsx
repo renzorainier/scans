3adf51aa8edd6c0a302bd3e3123320c4ce995f74
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-13 10:04:30 +0000

    Thu, Apr 13, 2023, 10:04 AM +00:00

--- components/TodayView.jsx
+++ components/TodayView.jsx
@@ -166,7 +166,7 @@ function AttendanceTable() {
             checked={state}
             onChange={setState}
             className={`${state ? "bg-gray-300" : "bg-gray-300"}
-    relative inline-flex h-[75px] w-full shrink-0 cursor-pointer rounded-lg border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
+    relative inline-flex h-[20px] w-full shrink-0 cursor-pointer rounded-lg border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
           >
             <span
               aria-hidden="true"
@@ -174,10 +174,8 @@ function AttendanceTable() {
       pointer-events-none mt-2  inline-block h-[80%] w-[50%] transform rounded-lg bg-white shadow-lg ring-0 transition duration-200 ease-in-out p-5`}
             />
             <div className="absolute top-1/2 left-[18%] transform -translate-y-1/2 ">
-              <SlGraph className="text-5xl" />
             </div>
             <div className="absolute top-1/2 right-[18%] transform -translate-y-1/2">
-              <FaAward className="text-5xl" />
             </div>
           </Switch>
         </div>
