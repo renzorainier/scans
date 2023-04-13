36fe15e8ceb786424341de5d5abf9dc036f57ac5
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-13 09:58:58 +0000

    Thu, Apr 13, 2023, 9:58 AM +00:00

--- components/Rank.jsx
+++ components/Rank.jsx
@@ -61,31 +61,31 @@ function Rank({ data, onClose }) {
      <div className="flex space-x-1 ">
   <button
     onClick={() => handleTabClick("A")}
-    className="w-full px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-blue-600 border border-transparent rounded-lg active:bg-blue-600 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue"
+    className="w-full px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-gray-300 border border-transparent rounded-lg active:bg-gray-300 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue"
   >
     Mon
   </button>
   <button
     onClick={() => handleTabClick("B")}
-    className="w-full px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-blue-600 border border-transparent rounded-lg active:bg-blue-600 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue"
+    className="w-full px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-gray-300 border border-transparent rounded-lg active:bg-gray-300 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue"
   >
     Tue
   </button>
   <button
     onClick={() => handleTabClick("C")}
-    className="w-full px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-blue-600 border border-transparent rounded-lg active:bg-blue-600 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue"
+    className="w-full px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-gray-300 border border-transparent rounded-lg active:bg-gray-300 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue"
   >
     Wed
   </button>
   <button
     onClick={() => handleTabClick("D")}
-    className="w-full px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-blue-600 border border-transparent rounded-lg active:bg-blue-600 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue"
+    className="w-full px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-gray-300 border border-transparent rounded-lg active:bg-gray-300 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue"
   >
     Thu
   </button>
   <button
     onClick={() => handleTabClick("E")}
-    className="w-full px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-blue-600 border border-transparent rounded-lg active:bg-blue-600 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue"
+    className="w-full px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-gray-300 border border-transparent rounded-lg active:bg-gray-300 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue"
   >
     Fri
   </button>
