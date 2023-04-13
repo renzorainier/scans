6c42d2d9a9cfcdae8a6049f3f68922689bcba1e9
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-13 09:53:07 +0000

    Thu, Apr 13, 2023, 9:53 AM +00:00

--- components/Select.jsx
+++ components/Select.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   Tabs,
   TabsHeader,
@@ -6,62 +7,42 @@ import {
   TabPanel,
 } from "@material-tailwind/react";
 
+
 export default function Example() {
   const data = [
     {
-      label: "HTML",
-      value: "html",
+      label: "Dashboard",
+      value: "dashboard",
       desc: `It really matters and then like it really doesn't matter.
       What matters is the people who are sparked by it. And the people
       who are like offended by it, it doesn't matter.`,
     },
     {
-      label: "React",
-      value: "react",
+      label: "Profile",
+      value: "profile",
       desc: `Because it's about motivating the doers. Because I'm here
       to follow my dreams and inspire other people to follow their dreams, too.`,
     },
-
     {
-      label: "Vue",
-      value: "vue",
-      desc: `We're not always in the position that we want to be at.
-      We're constantly growing. We're constantly making mistakes. We're
-      constantly trying to express ourselves and actualize our dreams.`,
-    },
-
-    {
-      label: "Angular",
-      value: "angular",
-      desc: `Because it's about motivating the doers. Because I'm here
-      to follow my dreams and inspire other people to follow their dreams, too.`,
-    },
-
-    {
-      label: "Svelte",
-      value: "svelte",
+      label: "Settings",
+      value: "settings",
       desc: `We're not always in the position that we want to be at.
       We're constantly growing. We're constantly making mistakes. We're
       constantly trying to express ourselves and actualize our dreams.`,
     },
   ];
-
   return (
-    <Tabs id="custom-animation" value="html">
+    <Tabs value="dashboard">
       <TabsHeader>
-        {data.map(({ label, value }) => (
+        {data.map(({ label, value, icon }) => (
           <Tab key={value} value={value}>
-            {label}
+            <div className="flex items-center gap-2">
+              {label}
+            </div>
           </Tab>
         ))}
       </TabsHeader>
-      <TabsBody
-        animate={{
-          initial: { y: 250 },
-          mount: { y: 0 },
-          unmount: { y: 250 },
-        }}
-      >
+      <TabsBody>
         {data.map(({ value, desc }) => (
           <TabPanel key={value} value={value}>
             {desc}
