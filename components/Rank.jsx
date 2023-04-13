643e820d30d9b16896685ea2cb0fda787fb38b41
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-13 09:58:11 +0000

    Thu, Apr 13, 2023, 9:58 AM +00:00

--- components/Rank.jsx
+++ components/Rank.jsx
@@ -58,7 +58,7 @@ function Rank({ data, onClose }) {
   return (
     <div>
       <div>
-     <div className="flex space-x-1">
+     <div className="flex space-x-1 ">
   <button
     onClick={() => handleTabClick("A")}
     className="w-full px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-blue-600 border border-transparent rounded-lg active:bg-blue-600 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue"
