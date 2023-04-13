4c22ef22d4f8f2986aaaa387dc9bcb32f935a009
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-13 10:01:59 +0000

    Thu, Apr 13, 2023, 10:01 AM +00:00

--- components/Rank.jsx
+++ components/Rank.jsx
@@ -61,31 +61,31 @@ function Rank({ data, onClose }) {
      <div className="flex space-x-1 ">
   <button
     onClick={() => handleTabClick("A")}
-    className="w-full px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-gray-300 border border-transparent rounded-lg active:bg-gray-300 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue"
+    className="w-full px-4 py-2  leading-5 text-white transition-colors duration-150 bg-gray-300 border border-transparent rounded-lg active:bg-gray-300 hover:bg-gray-700 focus:outline-none focus:shadow-outline-blue"
   >
     Mon
   </button>
   <button
     onClick={() => handleTabClick("B")}
-    className="w-full px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-gray-300 border border-transparent rounded-lg active:bg-gray-300 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue"
+    className="w-full px-4 py-2  leading-5 text-white transition-colors duration-150 bg-gray-300 border border-transparent rounded-lg active:bg-gray-300 hover:bg-gray-700 focus:outline-none focus:shadow-outline-blue"
   >
     Tue
   </button>
   <button
     onClick={() => handleTabClick("C")}
-    className="w-full px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-gray-300 border border-transparent rounded-lg active:bg-gray-300 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue"
+    className="w-full px-4 py-2  leading-5 text-white transition-colors duration-150 bg-gray-300 border border-transparent rounded-lg active:bg-gray-300 hover:bg-gray-700 focus:outline-none focus:shadow-outline-blue"
   >
     Wed
   </button>
   <button
     onClick={() => handleTabClick("D")}
-    className="w-full px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-gray-300 border border-transparent rounded-lg active:bg-gray-300 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue"
+    className="w-full px-4 py-2  leading-5 text-white transition-colors duration-150 bg-gray-300 border border-transparent rounded-lg active:bg-gray-300 hover:bg-gray-700 focus:outline-none focus:shadow-outline-blue"
   >
     Thu
   </button>
   <button
     onClick={() => handleTabClick("E")}
-    className="w-full px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-gray-300 border border-transparent rounded-lg active:bg-gray-300 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue"
+    className="w-full px-4 py-2  leading-5 text-white transition-colors duration-150 bg-gray-300 border border-transparent rounded-lg active:bg-gray-300 hover:bg-gray-700 focus:outline-none focus:shadow-outline-blue"
   >
     Fri
   </button>
@@ -127,9 +127,9 @@ export default Rank;
   /* <div>
 <div>
   <div className="fixed z-50 top-0 left-0 w-screen h-screen backdrop-blur-xl bg-gray/90"></div>
-  <div className="fixed z-50 top-1/2 left-1/2  transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-md shadow-xl p-8 w-4/5 h-full max-w-md">
+  <div className="fixed z-50 top-1/2anslate/2 -tranite  shadow-xl p-8 w-4/5 h-full max-w-md">
     <button
-      className="absolute top-3 right-3 text-gray-500 hover:text-gray-700"
+      className="absolute top-3 gray-3 gray-gray-500 hover:text-gray-700"
       onClick={onClose}
     >
       <svg
@@ -142,17 +142,17 @@ export default Rank;
         <path
           strokeLinecap="round"
           strokeLinejoin="round"
-          strokeWidth="2"
-          d="M6 18L18 6M6 6l12 12"
+          strokeW
+ 18L18 6M6 6l12 12"
         />
       </svg>
     </button>
     <h2>Overall Top 10 Earliest Students</h2>
-<ol>
+<gray>
   {overallEarliest.map(({ name, student, section,   formattedTime }) => (
     <li key={student}>{name}- {formattedTime} - {section}</li>
   ))}
-</ol>
+</gray>
 {Object.keys(earliestStudents).map((section) => (
   <div key={section}>
     <h2>{section}</h2>
