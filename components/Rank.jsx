b403e31b07e7bce1c9ba9965f43a04a1b8c3ca1c
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-13 14:22:39 +0000

    Thu, Apr 13, 2023, 2:22 PM +00:00

--- components/Rank.jsx
+++ components/Rank.jsx
@@ -3,13 +3,25 @@ import React, { useEffect, useRef, useState } from "react";
 function getBadgeRank(rank) {
   switch (rank) {
     case 1:
-      return 'gold';
+      return "1";
     case 2:
-      return 'silver';
+      return "2";
     case 3:
-      return 'bronze';
-    default:
-      return 'none';
+      return "3";
+    case 4:
+      return "4";
+    case 5:
+      return "5";
+    case 6:
+      return "6";
+    case 7:
+      return "7";
+    case 8:
+      return "8";
+    case 9:
+      return "9";
+    case 10:
+      return "10";
   }
 }
 
