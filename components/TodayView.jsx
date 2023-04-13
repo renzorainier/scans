6d4c4096c8b0bcdb550761bcfa8a92c2350b27e7
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-13 03:56:02 +0000

    Thu, Apr 13, 2023, 3:56 AM +00:00

--- components/TodayView.jsx
+++ components/TodayView.jsx
@@ -161,24 +161,25 @@ function AttendanceTable() {
     <div>
       <div className="flex justify-center pt-5 items-center">
         <div className="w-full rounded-lg mx-auto" style={{ maxWidth: "90%" }}>
-          <Switch
-            checked={state}
-            onChange={setState}
-            className={`${state ? "bg-gray-300" : "bg-gray-300"}
-        relative inline-flex h-[75px] w-full shrink-0 cursor-pointer rounded-lg border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
-          >
-            <span
-              aria-hidden="true"
-              className={`${state ? "translate-x-[100%]" : "translate-x-0"}
-          pointer-events-none inline-block  h-[80%] w-[50%] transform rounded-lg bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
-            />
-            <div className="absolute top-1/2 left-[18%] transform -translate-y-1/2 ">
-              <SlGraph className="text-5xl" />
-            </div>
-            <div className="absolute top-1/2 right-[18%] transform -translate-y-1/2">
-              <FaAward className="text-5xl" />
-            </div>
-          </Switch>
+        <Switch
+  checked={state}
+  onChange={setState}
+  className={`${state ? "bg-gray-300" : "bg-gray-300"}
+    relative inline-flex h-[75px] w-full shrink-0 cursor-pointer rounded-lg border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
+>
+  <span
+    aria-hidden="true"
+    className={`${state ? "translate-x-[100%]" : "translate-x-0"}
+      pointer-events-none inline-block h-[80%] w-[50%] transform rounded-lg bg-white shadow-lg ring-0 transition duration-200 ease-in-out p-5`}
+  />
+  <div className="absolute top-1/2 left-[18%] transform -translate-y-1/2 ">
+    <SlGraph className="text-5xl" />
+  </div>
+  <div className="absolute top-1/2 right-[18%] transform -translate-y-1/2">
+    <FaAward className="text-5xl" />
+  </div>
+</Switch>
+
         </div>
       </div>
 
