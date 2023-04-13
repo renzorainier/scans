85da2146c3d27c92248b284a9f9fb734d38de988
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-13 10:31:19 +0000

    Thu, Apr 13, 2023, 10:31 AM +00:00

--- components/Rank.jsx
+++ components/Rank.jsx
@@ -59,44 +59,43 @@ function Rank({ data, onClose }) {
     <div>
 
 
-      <div className="flex justify-center pt-5 pb-5 items-center">
-              <div
-                className="w-full text-gray-700 bg-white p-5  rounded-lg shadow-lg mx-auto"
-                style={{ maxWidth: "90%" }}
-              >        <div className="flex space-x-1 ">
-          <button
-            onClick={() => handleTabClick("A")}
-            className="w-full px-4 py-2  leading-5 text-white transition-colors duration-150 bg-gray-300 border border-transparent rounded-lg active:bg-gray-300 hover:bg-gray-400 focus:outline-none "
-          >
-            Mon
-          </button>
-          <button
-            onClick={() => handleTabClick("B")}
-            className="w-full px-4 py-2  leading-5 text-white transition-colors duration-150 bg-gray-300 border border-transparent rounded-lg active:bg-gray-300 hover:bg-gray-400 focus:outline-none "
-          >
-            Tue
-          </button>
-          <button
-            onClick={() => handleTabClick("C")}
-            className="w-full px-4 py-2  leading-5 text-white transition-colors duration-150 bg-gray-300 border border-transparent rounded-lg active:bg-gray-300 hover:bg-gray-400 focus:outline-none "
-          >
-            Wed
-          </button>
-          <button
-            onClick={() => handleTabClick("D")}
-            className="w-full px-4 py-2  leading-5 text-white transition-colors duration-150 bg-gray-300 border border-transparent rounded-lg active:bg-gray-300 hover:bg-gray-400 focus:outline-none "
-          >
-            Thu
-          </button>
-          <button
-            onClick={() => handleTabClick("E")}
-            className="w-full px-4 py-2  leading-5 text-white transition-colors duration-150 bg-gray-300 border border-transparent rounded-lg active:bg-gray-300 hover:bg-gray-400 focus:outline-none "
-          >
-            Fri
-          </button>
-        </div>
-        </div>
-        </div>
+<div className="flex justify-center pt-5 pb-5 items-center">
+  <div className="w-full text-gray-700 bg-white p-5 rounded-lg shadow-lg mx-auto" style={{ maxWidth: "90%" }}>
+    <div className="flex space-x-2">
+      <button
+        onClick={() => handleTabClick("A")}
+        className="w-full px-4 py-2 text-white transition-colors duration-150 bg-gray-300 border border-transparent rounded-lg active:bg-gray-300 hover:bg-gray-400 focus:outline-none"
+      >
+        Mon
+      </button>
+      <button
+        onClick={() => handleTabClick("B")}
+        className="w-full px-4 py-2 text-white transition-colors duration-150 bg-gray-300 border border-transparent rounded-lg active:bg-gray-300 hover:bg-gray-400 focus:outline-none"
+      >
+        Tue
+      </button>
+      <button
+        onClick={() => handleTabClick("C")}
+        className="w-full px-4 py-2 text-white transition-colors duration-150 bg-gray-300 border border-transparent rounded-lg active:bg-gray-300 hover:bg-gray-400 focus:outline-none"
+      >
+        Wed
+      </button>
+      <button
+        onClick={() => handleTabClick("D")}
+        className="w-full px-4 py-2 text-white transition-colors duration-150 bg-gray-300 border border-transparent rounded-lg active:bg-gray-300 hover:bg-gray-400 focus:outline-none"
+      >
+        Thu
+      </button>
+      <button
+        onClick={() => handleTabClick("E")}
+        className="w-full px-4 py-2 text-white transition-colors duration-150 bg-gray-300 border border-transparent rounded-lg active:bg-gray-300 hover:bg-gray-400 focus:outline-none"
+      >
+        Fri
+      </button>
+    </div>
+  </div>
+</div>
+
 
       <div className="flex justify-center pt-5 pb-5 items-center">
               <div
