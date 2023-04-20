cedfd9306277b2dcc3c483818003979f420ff51f
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-20 15:39:22 +0000

    Thu, Apr 20, 2023, 3:39 PM +00:00

--- components/Rank.jsx
+++ components/Rank.jsx
@@ -149,7 +149,7 @@ function Rank({ data, onClose }) {
 
       <div className="flex justify-center items-center">
         <div
-          className="w-full text-gray-70 flex justify-center font-bold items-center bg-gray- p-5 rounded-t-lg shadow-lg mx-auto"
+          className="w-full text-gray-70 flex justify-center font-bold items-center bg-white p-5 rounded-t-lg shadow-lg mx-auto"
           style={{ maxWidth: "90%" }}
         >
           Overall Top 10 Most Punctual Students
