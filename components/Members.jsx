5323d3ca8616ca06420314894a36d2e2806e68b1
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-21 16:17:34 +0000

    Fri, Apr 21, 2023, 4:17 PM +00:00

--- components/Members.jsx
+++ components/Members.jsx
@@ -1,22 +1,24 @@
-import React from "react";
+import { useState, useEffect, useRef } from "react";
 
 const teamMembers = [
   { name: "John Doe", imageUrl: "/pictures/pic.jpg" },
   { name: "Jane Smith", imageUrl: "/pictures/2.png" },
   { name: "Bob Johnson", imageUrl: "/pictures/3.png" },
-  { name: "Samuel Lee", imageUrl: "/pictures/4.png" },
-  { name: "Emma Watson", imageUrl: "/pictures/5.png" },
-  { name: "Peter Parker", imageUrl: "/pictures/6.png" },
+  { name: "Alice Lee", imageUrl: "/pictures/4.png" },
+  { name: "Peter Parker", imageUrl: "/pictures/5.png" },
+  { name: "Mary Jane", imageUrl: "/pictures/6.png" },
 ];
 
 function TeamMembers() {
   return (
     <div className="grid grid-cols-4 gap-4">
       {teamMembers.map((member, index) => (
-        <div key={index} className="bg-white shadow-lg rounded-lg overflow-hidden">
-          <img className="w-full h-full object-cover object-center" src={member.imageUrl} alt={member.name} />
+        <div key={index} className="bg-white shadow-lg rounded-lg overflow-hidden flex flex-col items-center">
+          <div className="rounded-full w-36 h-36 overflow-hidden mt-6">
+            <img className="w-full h-full object-cover object-center" src={member.imageUrl} alt={member.name} />
+          </div>
           <div className="p-4">
-            <h2 className="font-bold text-lg">{member.name}</h2>
+            <h2 className="font-bold text-lg mt-4">{member.name}</h2>
           </div>
         </div>
       ))}
