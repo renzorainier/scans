607b098c7cb2c3c9e5e41981dde711bfbd2d0cf3
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-21 15:26:19 +0000

    Fri, Apr 21, 2023, 3:26 PM +00:00

--- components/Members.jsx
+++ components/Members.jsx
@@ -1,30 +1,38 @@
-import { useState, useEffect, useRef } from "react";
+import { useState } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const teamMembers = [
   { name: "John Doe", imageUrl: "/pictures/1.png" },
   { name: "Jane Smith", imageUrl: "/pictures/2.png" },
   { name: "Bob Johnson", imageUrl: "/pictures/3.png" },
+  { name: "Jane Doe", imageUrl: "/pictures/4.png" },
+  { name: "Bob Smith", imageUrl: "/pictures/5.png" },
 ];
 
 function TeamCarousel() {
-  const [currentMemberIndex, setCurrentMemberIndex] = useState(0);
-  const containerRef = useRef(null);
+  const [currentSlide, setCurrentSlide] = useState(0);
 
-  useEffect(() => {
-    const container = containerRef.current;
-    const memberWidth = container.querySelector(".team-member").offsetWidth;
-    let intervalId = setInterval(() => {
-      setCurrentMemberIndex((currentMemberIndex + 1) % teamMembers.length);
-      container.style.transform = `translateX(-${currentMemberIndex * memberWidth}px)`;
-    }, 3000);
-    return () => clearInterval(intervalId);
-  }, [currentMemberIndex]);
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: "0px",
+    beforeChange: (current, next) => setCurrentSlide(next),
+    autoplay: true,
+    autoplaySpeed: 3000,
+    cssEase: "linear",
+  };
 
   return (
     <div className="relative overflow-hidden">
-      <div className="flex" ref={containerRef}>
+      <Slider {...settings}>
         {teamMembers.map((member, index) => (
-          <div key={index} className="team-member flex-shrink-0 w-full h-64">
+          <div key={index} className="team-member w-full h-64">
             <div
               className="h-full bg-cover bg-center"
               style={{ backgroundImage: `url(${member.imageUrl})` }}
@@ -34,6 +42,11 @@ function TeamCarousel() {
             </div>
           </div>
         ))}
+      </Slider>
+      <div className="absolute bottom-0 left-0 right-0 flex justify-center">
+        <div className="text-gray-600">
+          {currentSlide + 1} / {teamMembers.length}
+        </div>
       </div>
     </div>
   );
