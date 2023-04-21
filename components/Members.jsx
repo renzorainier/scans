b4764e628f68dfeb234b8305b4f1ca608718e053
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-21 16:20:46 +0000

    Fri, Apr 21, 2023, 4:20 PM +00:00

--- components/Members.jsx
+++ components/Members.jsx
@@ -11,19 +11,21 @@ const teamMembers = [
 
 function TeamMembers() {
   return (
-    <div className="grid grid-cols-4 gap-4">
+    <div className="grid grid-cols-3 md:grid-cols-4 gap-4">
       {teamMembers.map((member, index) => (
         <div key={index} className="bg-white shadow-lg rounded-lg overflow-hidden flex flex-col items-center">
           <div className="rounded-full w-36 h-36 overflow-hidden mt-6">
             <img className="w-full h-full object-cover object-center" src={member.imageUrl} alt={member.name} />
           </div>
           <div className="p-4">
-            <h2 className="font-bold text-lg mt-4">{member.name}</h2>
+            <h2 className="font-bold text-base mt-4">{member.name}</h2>
           </div>
         </div>
       ))}
     </div>
   );
+
+
 }
 
 export default TeamMembers;
