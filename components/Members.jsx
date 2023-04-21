b880a9cbf40b6fbc74d8f7419ca0320a3808177b
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-21 16:02:00 +0000

    Fri, Apr 21, 2023, 4:02 PM +00:00

--- components/Members.jsx
+++ components/Members.jsx
@@ -4,9 +4,9 @@ const teamMembers = [
   { name: "John Doe", imageUrl: "/pictures/1.png" },
   { name: "Jane Smith", imageUrl: "/pictures/2.png" },
   { name: "Bob Johnson", imageUrl: "/pictures/3.png" },
-    { name: " Doe", imageUrl: "/pictures/4.png" },
-  { name: " Smith", imageUrl: "/pictures/5.png" },
-  { name: " Johnson", imageUrl: "/pictures/6.png" },
+  { name: "Doe", imageUrl: "/pictures/4.png" },
+  { name: "Smith", imageUrl: "/pictures/5.png" },
+  { name: "Johnson", imageUrl: "/pictures/6.png" },
 ];
 
 function TeamCarousel() {
@@ -27,13 +27,13 @@ function TeamCarousel() {
     <div className="relative overflow-hidden">
       <div className="flex" ref={stripRef}>
         {teamMembers.map((member, index) => (
-          <div key={index} className="team-member flex-shrink-0 w-full h-64">
+          <div key={index} className="team-member flex-shrink-0 w-32 h-32 sm:w-40 sm:h-40 md:w-48 md:h-48 lg:w-56 lg:h-56 xl:w-64 xl:h-64">
             <div
               className="h-full bg-cover bg-center"
               style={{ backgroundImage: `url(${member.imageUrl})` }}
             ></div>
-            <div className="bg-black bg-opacity-50 p-4">
-              <h2 className="text-white text-lg font-bold">{member.name}</h2>
+            <div className="bg-black bg-opacity-50 p-2 sm:p-3 md:p-4">
+              <h2 className="text-white text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl font-bold">{member.name}</h2>
             </div>
           </div>
         ))}
