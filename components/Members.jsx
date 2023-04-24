8abc48eeef595b23b54f67f8265351e793f82da5
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-24 04:23:53 +0000

    Mon, Apr 24, 2023, 4:23 AM +00:00

--- components/Members.jsx
+++ components/Members.jsx
@@ -1,33 +1,86 @@
-import { useState, useEffect, useRef } from "react";
+import React from "react";
+import Image from "next/image";
 
-const teamMembers = [
-  { name: "John Doe", imageUrl: "/pictures/pic.jpg" },
-  { name: "Jane Smith", imageUrl: "/pictures/pic.jpg" },
-  { name: "Bob Johnson", imageUrl: "/pictures/pic.jpg" },
-  { name: "Alice Lee", imageUrl: "/pictures/pic.jpg" },
-  { name: "Peter Parker", imageUrl: "/pictures/pic.jpg" },
-  { name: "Mary Jane", imageUrl: "/pictures/pic.jpg" },
-];
+import renz from "./../public/pictures/pic.jpg";
+import celine from "./../public/pictures/pic.jpg";
+import xedrick from "./../public/pictures/pic.jpg";
+import aidrian from "./../public/pictures/pic.jpg";
+import klaus from "./../public/pictures/pic.jpg";
+import marjorie from "./../public/pictures/pic.jpg";
 
 function TeamMembers() {
   return (
-    
-    <div className="grid grid-cols-3 gap-4">
-    {teamMembers.map((member, index) => (
-      <div key={index} className="bg-white shadow-lg rounded-lg overflow-hidden flex flex-col items-center">
-        <div className="rounded-full w-36 h-36 overflow-hidden mt-6">
-          <img className="w-full h-full object-cover object-center" src={member.imageUrl} alt={member.name} />
+    <div className="grid grid-cols-3 md:grid-cols-6 gap-4">
+      <div className="flex flex-col items-center">
+        <div className="w-12 h-12 md:w-20 md:h-20 rounded-full overflow-hidden">
+          <Image className="object-cover w-full h-full" src={renz} alt="Renz" />
         </div>
-        <div className="p-4">
-          <h2 className="font-bold text-lg mt-4">{member.name}</h2>
-        </div>
+        <h2 className="font-bold text-base md:text-lg mt-2 md:mt-4">
+          Renz Doe
+        </h2>
       </div>
-    ))}
-  </div>
-
+      <div className="flex flex-col items-center">
+        <div className="w-12 h-12 md:w-20 md:h-20 rounded-full overflow-hidden">
+          <Image
+            className="object-cover w-full h-full"
+            src={celine}
+            alt="Celine"
+          />
+        </div>
+        <h2 className="font-bold text-base md:text-lg mt-2 md:mt-4">
+          Celine Doe
+        </h2>
+      </div>
+      <div className="flex flex-col items-center">
+        <div className="w-12 h-12 md:w-20 md:h-20 rounded-full overflow-hidden">
+          <Image
+            className="object-cover w-full h-full"
+            src={xedrick}
+            alt="Xedrick"
+          />
+        </div>
+        <h2 className="font-bold text-base md:text-lg mt-2 md:mt-4">
+          Xedrick Doe
+        </h2>
+      </div>
+      <div className="flex flex-col items-center">
+        <div className="w-12 h-12 md:w-20 md:h-20 rounded-full overflow-hidden">
+          <Image
+            className="object-cover w-full h-full"
+            src={aidrian}
+            alt="Aidrian"
+          />
+        </div>
+        <h2 className="font-bold text-base md:text-lg mt-2 md:mt-4">
+          Aidrian Doe
+        </h2>
+      </div>
+      <div className="flex flex-col items-center">
+        <div className="w-12 h-12 md:w-20 md:h-20 rounded-full overflow-hidden">
+          <Image
+            className="object-cover w-full h-full"
+            src={klaus}
+            alt="Klaus"
+          />
+        </div>
+        <h2 className="font-bold text-base md:text-lg mt-2 md:mt-4">
+          Klaus Doe
+        </h2>
+      </div>
+      <div className="flex flex-col items-center">
+        <div className="w-12 h-12 md:w-20 md:h-20 rounded-full overflow-hidden">
+          <Image
+            className="object-cover w-full h-full"
+            src={marjorie}
+            alt="Marjorie"
+          />
+        </div>
+        <h2 className="font-bold text-base md:text-lg mt-2 md:mt-4">
+          Marjorie Doe
+        </h2>
+      </div>
+    </div>
   );
-
-
 }
 
 export default TeamMembers;
