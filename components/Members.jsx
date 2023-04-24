a39e2438d964c00a0e2c5792b76fc34884b1d6ba
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-24 04:54:35 +0000

    Mon, Apr 24, 2023, 4:54 AM +00:00

--- components/Members.jsx
+++ components/Members.jsx
@@ -10,98 +10,49 @@ import marjorie from "./../public/pictures/pic.jpg";
 
 function TeamMembers() {
   return (
-    <div className="grid grid-cols-3 md:grid-cols-6">
-      <div className="flex flex-col items-center">
-        <div className="w-20 h-20 md:w-20 md:h-20 rounded-full overflow-hidden">
-          <Image className="object-cover w-full h-full" src={renz} alt="Renz" />
-        </div>
-        <h2 className="font-bold text-base md:text-lg mt-2 md:mt-4">
-          Renz Pasagdan
-        </h2>
-          <h2 className=" text-base md:text-lg  md:mt-4">
-          Leader
-        </h2>
-          <h2 className=" text-base md:text-lg  md:mt-4">
-          Programmer
-        </h2>
-      </div>
-      <div className="flex flex-col items-center">
-        <div className="w-20 h-20 md:w-20 md:h-20 rounded-full overflow-hidden">
-          <Image
-            className="object-cover w-full h-full"
-            src={celine}
-            alt="Celine"
-          />
-        </div>
-        <h2 className="font-bold text-base md:text-lg mt-2 md:mt-4">
-          Celine Mendi
-        </h2>
-        <h2 className=" text-base md:text-lg  md:mt-4">
-          Researcher
-        </h2>
+    <div className="grid grid-cols-2 gap-4 md:grid-cols-6">
+      <TeamMember
+        name="Renz Pasagdan"
+        role="Leader, Programmer"
+        image={renz}
+      />
+      <TeamMember
+        name="Celine Mendi"
+        role="Researcher"
+        image={celine}
+      />
+      <TeamMember
+        name="Xedrick Pureza"
+        role="Researcher"
+        image={xedrick}
+      />
+      <TeamMember
+        name="Adrian Bataller"
+        role="Researcher"
+        image={aidrian}
+      />
+      <TeamMember
+        name="Klaus Lonquino"
+        role="Researcher"
+        image={klaus}
+      />
+      <TeamMember
+        name="Marjorie Polinar"
+        role="Researcher"
+        image={marjorie}
+      />
+    </div>
+  );
+}
 
+function TeamMember({ name, role, image }) {
+  return (
+    <div className="flex flex-col items-center">
+      <div className="w-24 h-24 md:w-32 md:h-32 rounded-full overflow-hidden">
+        <Image className="object-cover w-full h-full" src={image} alt={name} />
       </div>
-      <div className="flex flex-col items-center">
-        <div className="w-20 h-20 md:w-20 md:h-20 rounded-full overflow-hidden">
-          <Image
-            className="object-cover w-full h-full"
-            src={xedrick}
-            alt="Xedrick"
-          />
-        </div>
-        <h2 className="font-bold text-base md:text-lg mt-2 md:mt-4">
-          Xedrick Pureza
-        </h2>
-        <h2 className=" text-base md:text-lg  md:mt-4">
-          Researcher
-        </h2>
-
-      </div>
-      <div className="flex flex-col items-center">
-        <div className="w-20 h-20 md:w-20 md:h-20 rounded-full overflow-hidden">
-          <Image
-            className="object-cover w-full h-full"
-            src={aidrian}
-            alt="Aidrian"
-          />
-        </div>
-        <h2 className="font-bold text-base md:text-lg mt-2 md:mt-4">
-          Adrian Bataller
-        </h2>
-        <h2 className=" text-base md:text-lg  md:mt-4">
-          Researcher
-        </h2>
-      </div>
-      <div className="flex flex-col items-center">
-        <div className="w-20 h-20 md:w-20 md:h-20 rounded-full overflow-hidden">
-          <Image
-            className="object-cover w-full h-full"
-            src={klaus}
-            alt="Klaus"
-          />
-        </div>
-        <h2 className="font-bold text-base md:text-lg mt-2 md:mt-4">
-          Klaus Lonquino
-        </h2>
-        <h2 className=" text-base md:text-lg  md:mt-4">
-          Researcher
-        </h2>
-      </div>
-      <div className="flex flex-col items-center">
-        <div className="w-20 h-20 md:w-20 md:h-20 rounded-full overflow-hidden">
-          <Image
-            className="object-cover w-full h-full"
-            src={marjorie}
-            alt="Marjorie"
-          />
-        </div>
-        <h2 className="font-bold text-base md:text-lg mt-2 md:mt-4">
-          Marjorie Polinar
-        </h2>
-        <h2 className=" text-base md:text-lg  md:mt-4">
-          Researcher
-        </h2>
-      </div>
+      <h2 className="font-bold text-lg mt-2 md:text-xl md:mt-4">{name}</h2>
+      <h3 className="text-base md:text-lg md:mt-2">{role}</h3>
     </div>
   );
 }
