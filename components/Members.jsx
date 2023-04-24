6debd9b371eb9e9888cfb40c2dbd421b72312252
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-24 12:12:33 +0000

    Mon, Apr 24, 2023, 12:12 PM +00:00

--- components/Members.jsx
+++ components/Members.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Image from "next/image";
 
-import renz from "./../public/pictures/pic.jpg";
-import celine from "./../public/pictures/pic.jpg";
-import xedrick from "./../public/pictures/pic.jpg";
-import aidrian from "./../public/pictures/pic.jpg";
+import renz from "./../public/pictures/renz.webp";
+import celine from "./../public/pictures/celine.webp";
+import xedrick from "./../public/pictures/xed.webp";
+import aidrian from "./../public/pictures/adraian";
 import klaus from "./../public/pictures/pic.jpg";
 import marjorie from "./../public/pictures/pic.jpg";
 
