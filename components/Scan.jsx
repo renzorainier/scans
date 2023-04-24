7e04f6470803d8d0dc150a7435bd2ca01fa9a294
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-24 14:09:18 +0000

    Mon, Apr 24, 2023, 2:09 PM +00:00

--- components/Scan.jsx
+++ components/Scan.jsx
@@ -295,7 +295,7 @@ function Scan() {
 
   return (
     <div className="bg-gray-100 min-h-screen flex flex-col items-center justify-center">
-      <div className="bg-white rounded-lg shadow-md p-6 w-full max-w-md">
+      <div className="bg-white rounded-lg shadow-md p-6 w-full h-full max-w-md">
         <QrReader
           onResult={async (result) => {
             if (!!result) {
@@ -317,7 +317,7 @@ function Scan() {
         />
         <p className="text-xl font-bold mt-6">Scan result:</p>
         <p className="text-xl">{data}</p>
-        <h1 className="text-3xl font-semibold mt-8">Recent Scans:</h1>
+        <h1 className="text-3xl font-semibold mt-8">Recent Scans</h1>
         <div className="bg-white rounded-lg shadow-lg mt-6 w-full max-w-md overflow-y-scroll">
           <ul className="text-gray-500 divide-y divide-gray-300">
             {log.map((entry, index) => (
