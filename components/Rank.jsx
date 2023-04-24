1a9afc101c5e249672f20863c1b79e6dc2ed23fa
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-24 04:31:23 +0000

    Mon, Apr 24, 2023, 4:31 AM +00:00

--- components/Rank.jsx
+++ components/Rank.jsx
@@ -107,7 +107,7 @@ function Rank({ data, onClose }) {
 
 
   return (
-    <div>
+    <div id="badge">
      <div className="flex justify-center pt-4 pb-5 items-center">
   <div className="bg-white p-5 rounded-lg shadow-lg mx-auto" style={{ maxWidth: "90%" }}>
     <div className="flex space-x-2">
