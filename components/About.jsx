8992beb807334b10b866fa423e0f67b03965a7a9
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-24 09:58:16 +0000

    Mon, Apr 24, 2023, 9:58 AM +00:00

--- components/About.jsx
+++ components/About.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { FaChevronDown } from 'react-icons/fa';
+import React, { useState } from "react";
+import { FaChevronDown } from "react-icons/fa";
 
 const About = () => {
   const [card1Expanded, setCard1Expanded] = useState(false);
@@ -29,14 +29,17 @@ const About = () => {
                 <div className="flex items-center">
                   <FaChevronDown
                     className={`transition-transform ${
-                      card1Expanded ? 'rotate-180' : ''
+                      card1Expanded ? "rotate-180" : ""
                     }`}
                   />
                 </div>
               </div>
               {card1Expanded && (
                 <div className="mt-2 text-sm text-gray-500">
-                  Scans is a web application developed by a Grade 12 ST-1B student of Sumulong College of Arts and Sciences as a research project titled "Development of QR Code Technology as an Attendance System for Enhancing Attendance and Punctuality"
+                  Scans is a web application developed by a Grade 12 ST-1B
+                  student of Sumulong College of Arts and Sciences as a research
+                  project titled "Development of QR Code Technology as an
+                  Attendance System for Enhancing Attendance and Punctuality"
                 </div>
               )}
             </div>
@@ -54,14 +57,19 @@ const About = () => {
                 <div className="flex items-center">
                   <FaChevronDown
                     className={`transition-transform ${
-                      card2Expanded ? 'rotate-180' : ''
+                      card2Expanded ? "rotate-180" : ""
                     }`}
                   />
                 </div>
               </div>
               {card2Expanded && (
                 <div className="mt-2 text-sm text-gray-500">
-                  Our mission is to improve student attendance and punctuality by utilizing QR code technology and psychological theories to create an efficient and reliable attendance tracking system. We strive to empower students, teachers, and parents with real-time attendance data to foster a culture of accountability and academic excellence.
+                  Our mission is to improve student attendance and punctuality
+                  by utilizing QR code technology and psychological theories to
+                  create an efficient and reliable attendance tracking system.
+                  We strive to empower students, teachers, and parents with
+                  real-time attendance data to foster a culture of
+                  accountability and academic excellence.
                 </div>
               )}
             </div>
@@ -75,25 +83,32 @@ const About = () => {
               <div className="flex justify-between items-center">
                 <h3 className="text-lg leading-6 font-medium text-gray-900">
                   Who Will Benefit?
-              </h3>
-
+                </h3>
+                <div className="flex items-center">
+                  <FaChevronDown
+                    className={`transition-transform ${
+                      card2Expanded ? "rotate-180" : ""
+                    }`}
+                  />
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      </div>
-      </div>
+    </div>
   );
 };
 
 export default About;
 
-
-{/* <div className="mt-2 text-sm text-gray-500">
+{
+  /* <div className="mt-2 text-sm text-gray-500">
 <ul className="list-disc list-inside">
   <li>Students: Motivates attendance and punctuality, recognizes and rewards positive behavior through digital achievement badges.</li>
   <li>Teachers: Streamlines administrative tasks, enhances attendance tracking and monitoring, enabling more time for teaching.</li>
   <li>Parents/Guardians: Offers timely attendance data for their child, allowing for better involvement in their education.</li>
   <li>School: Cultivates a culture of responsibility and academic success, advances student participation and lowers absenteeism rates.</li>
 </ul>
-</div> */}
+</div> */
+}
