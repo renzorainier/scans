aba02d3d16358de3e2ca9b8601eb586e3da53606
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-24 14:45:05 +0000

    Mon, Apr 24, 2023, 2:45 PM +00:00

--- components/Pass.jsx
+++ components/Pass.jsx
@@ -1,9 +1,8 @@
-import { useState } from 'react';
+import { useState } from "react";
 import Scan from "./Scan";
 
-
 function PasswordProtectedContent() {
-  const [password, setPassword] = useState('');
+  const [password, setPassword] = useState("");
   const [showContent, setShowContent] = useState(false);
 
   const handlePasswordChange = (event) => {
@@ -13,41 +12,48 @@ function PasswordProtectedContent() {
   const handleFormSubmit = (event) => {
     event.preventDefault();
     // Replace this with your actual password check logic
-    if (password === 'password') {
+    if (password === "password") {
       setShowContent(true);
     }
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      {!showContent && (
-        <form onSubmit={handleFormSubmit} className="bg-white p-6 rounded-lg shadow-lg">
-          <label htmlFor="password" className="block font-medium text-gray-700 mb-2">
-            Password:
-          </label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={handlePasswordChange}
-            className="border border-gray-300 rounded-lg px-3 py-2 w-full mb-4"
-          />
-          <button
-            type="submit"
-            className="bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600"
+    <div>
+      <div className="flex flex-col items-center justify-center h-screen">
+        {!showContent && (
+          <form
+            onSubmit={handleFormSubmit}
+            className="bg-white p-6 rounded-lg shadow-lg"
           >
-            Submit
-          </button>
-        </form>
-      )}
+            <label
+              htmlFor="password"
+              className="block font-medium text-gray-700 mb-2"
+            >
+              Password:
+            </label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={handlePasswordChange}
+              className="border border-gray-300 rounded-lg px-3 py-2 w-full mb-4"
+            />
+            <button
+              type="submit"
+              className="bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600"
+            >
+              Submit
+            </button>
+          </form>
+        )}
+      </div>
       {showContent && (
         <div>
-          <Scan/>
+          <Scan />
         </div>
       )}
     </div>
   );
 }
-
 
 export default PasswordProtectedContent;
