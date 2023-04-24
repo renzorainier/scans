8cb8ba93fd7ca67bf313ca672655d03e943f24ab
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-24 12:47:18 +0000

    Mon, Apr 24, 2023, 12:47 PM +00:00

--- components/TodayView.jsx
+++ components/TodayView.jsx
@@ -191,7 +191,7 @@ function AttendanceTable() {
             </div>
           ) : (
             <div>
-              <div className="flex justify-center pt-4 pb-4 items-center">
+              <div className="flex justify-center pt-4 pb-5 items-center">
                 <div
                   className="w-full text-gray-700 bg-white p-5  rounded-lg shadow-lg mx-auto"
                   style={{ maxWidth: "90%" }}
