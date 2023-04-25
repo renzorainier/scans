45b34ab98518017f1fac4231c2eb1f10d5729ea3
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-25 23:23:30 +0000

    Tue, Apr 25, 2023, 11:23 PM +00:00

--- components/Rank.jsx
+++ components/Rank.jsx
@@ -234,31 +234,19 @@ function Rank({ data, onClose }) {
                     >
                       {earliestStudents[section].map(
                         ({ name, student, formattedTime }, index) => (
-                          <div
-                            key={student}
-                            className="relative border border-white-200 rounded-lg overflow-hidden"
-                          >
-                            <div className="absolute top-0 left-0 right-0 p-2 bg-gradient-to-b from-violet-900 to-transparent">
-                              <div className="text-white text-lg">{name}</div>
-                              <div className="text-white text-sm">
-                                {formattedTime}&emsp;|&emsp;{section}
-                              </div>
-                            </div>
-                            <div className="w-full h-52 overflow-hidden">
-                            <Image
-  src={`/pictures/${getBadgeRankSec(index + 1)}.png`}
-  alt={`Badge ${getBadgeRankSec(index + 1)}`}
-  layout="fill"
-  objectFit="cover"
-/>
-
-                            </div>
-                            <div className="absolute top-0 right-0 p-2">
-                              <div className="text-white text-3xl font-bold">
-                                {index + 1}
-                              </div>
+                          <div key={student} className="relative border border-white-200 rounded-lg overflow-hidden">
+                          <div className="absolute top-0 left-0 right-0 bottom-0 p-2 bg-gradient-to-b from-violet-900 to-transparent">
+                            <div className="text-white text-lg">{name}</div>
+                            <div className="text-white text-sm">{formattedTime}&emsp;|&emsp;{section}</div>
+                          </div>
+                          <div className="w-full h-52 relative">
+                            <Image src={`/pictures/${getBadgeRankSec(index + 1)}.png`} alt={`Badge ${getBadgeRankSec(index + 1)}`} layout="fill" objectFit="cover" />
+                            <div className="absolute top-0 right-0 p-2 z-10">
+                              <div className="text-white text-3xl font-bold">{index + 1}</div>
                             </div>
                           </div>
+                        </div>
+
                         )
                       )}
                     </div>
