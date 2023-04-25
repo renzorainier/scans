6959f27b6ef9d452b22cf0a90f00461ecf9ca0da
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-25 14:36:12 +0000

    Tue, Apr 25, 2023, 2:36 PM +00:00

--- components/Main.jsx
+++ components/Main.jsx
@@ -81,6 +81,69 @@ const MainComponent = () => {
 
 export default MainComponent;
 
+
+
+const [selectedTab, setSelectedTab] = useState("c");
+
+const handleTabChange = (tab) => {
+  setSelectedTab(tab);
+};
+
+return (
+  <div>
+    <div className="flex justify-center pt-5 space-x-5 mb-4  ">
+      <button
+        className={`py-2 px-4 rounded-lg  ${
+          selectedTab === "a"
+            ? "bg-blue-400 text-white"
+            : "bg-gray-200 hover:bg-blue-200 transition duration-700 ease-in-out"
+        }`}
+        onClick={() => handleTabChange("a")}
+      >
+        <div className="hover:scale-150 ease-in duration-200">
+          h
+        </div>
+      </button>
+      <button
+        className={`py-2 px-4 rounded-lg ${
+          selectedTab === "b"
+            ? "bg-blue-400 text-white"
+            : "bg-gray-200 hover:bg-blue-200 transition duration-700 ease-in-out"
+        }`}
+        onClick={() => handleTabChange("b")}
+      >
+        <div className="hover:scale-150 ease-in duration-200">
+l
+        </div>
+      </button>
+      <button
+        className={`py-2 px-4 rounded-lg ${
+          selectedTab === "c"
+            ? "bg-blue-400 text-white"
+            : "bg-gray-200 hover:bg-blue-200 transition duration-700 ease-in-out"
+        }`}
+        onClick={() => handleTabChange("c")}
+      >
+        <div className="hover:scale-150 ease-in duration-200">
+          <AiOutlineUnorderedList />
+        </div>
+      </button>
+    </div>
+    <div className={`${selectedTab === "a" ? "block" : "hidden-component"}`}>
+      <PastAttendance className="tab-content" />
+    </div>
+    {/* <div className={`${selectedTab === "b" ? "block" : "hidden-component"}`}>
+      <LineGraph className="tab-content" />
+    </div> */}
+    <div className={`${selectedTab === "c" ? "block" : "hidden-component"}`}>
+      <TodayAttendance className="tab-content" />
+    </div>
+  </div>
+);
+}
+
+
+
 // <button className="bg-white font-bold text-xl py-10 rounded-lg " onClick={() => handleButtonClick("create")}>
 // Create
 // </button>
