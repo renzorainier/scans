4bbbcf21c207d5fb91cc48dfe45af2a9ed912f1e
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-04-25 08:29:53 +0000

    Tue, Apr 25, 2023, 8:29 AM +00:00

--- components/Members.jsx
+++ components/Members.jsx
@@ -52,7 +52,7 @@ function TeamMember({ name, role, image }) {
       <div className="w-24 h-24 md:w-32 md:h-32 rounded-full overflow-hidden">
         <Image className="object-cover w-full h-full" src={image} alt={name} />
       </div>
-      <h2 className="font-bold text-lg mt-2 md:text-xl md:mt-4">{name}</h2>
+      <h2 className="font-bold text-lg mt-2 md:text-xl md:mt-4"><span class="bg-gradient-to-r from-blue-400 to-violet-400 text-transparent bg-clip-text">{name}</span> </h2>
       <h3 className="text-sm md:text-lg md:mt-2">{role}</h3>
     </div>
   );
