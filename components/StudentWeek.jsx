81f048d28377222e5d5c657f6371f10bd439defe
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-05-04 09:06:20 +0000

    Thu, May 4, 2023, 9:06 AM +00:00

--- components/StudentWeek.jsx
+++ components/StudentWeek.jsx
@@ -4,7 +4,7 @@ function StudentChart({ student, onClose }) {
 
   return (
     <div>
-      <div className="fixed z-50 top-0 left-0 w-full h-full backdrop-blur-xl bg-gray/900"></div>
+<div className="fixed z-50 top-0 left-0 w-full h-full backdrop-blur-xl "></div>
       <div className="fixed z-50 top-1/2 left-1/2  transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-md shadow-xl p-8 w-4/5 max-w-md">
         <button
           className="absolute top-3 right-3 text-gray-500 hover:text-gray-700"
