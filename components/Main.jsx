bccc4274207931f43e0081531d11faf65951124d
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-05-04 09:30:23 +0000

    Thu, May 4, 2023, 9:30 AM +00:00

--- components/Main.jsx
+++ components/Main.jsx
@@ -8,7 +8,7 @@ import Pass from "./Pass";
 import Generate from "./Generate";
 import TeamCarousel from "./Members";
 import About from "./About";
-import { MdOutlineArrowBackIos }from "react-icons/io";
+import { FaChevronDown } from "react-icons/fa";
 
 const MainComponent = () => {
   const [currentComponent, setCurrentComponent] = useState(null);
@@ -67,7 +67,7 @@ const MainComponent = () => {
       className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline"
       onClick={handleBackButtonClick}
     >
-      <IoCaretBack/>
+      <FaChevronDown/>
     </button>
   ) : null;
 
