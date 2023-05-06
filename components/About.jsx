8864e1162053308918396fe363ebc9c37a166c63
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-05-06 03:50:57 +0000

    Sat, May 6, 2023, 3:50 AM +00:00

--- components/About.jsx
+++ components/About.jsx
@@ -119,29 +119,34 @@ const About = () => {
                 </div>
               </div>
               {card5Expanded && (
-          <div className="mt-2 text-sm text-gray-500">
-          <ul>
-            <li>
-            <span class="bg-gradient-to-r text-xl from-blue-400 to-violet-400 text-transparent bg-clip-text">
-                        {" "}
-                        Students:
-                      </span
-              Scan QR Code at School Gate: Students scan their unique QR codes at the
-              school gate using a scanning device.
-            </li>
-            <li>
-              Mark Attendance and Color Code: The system marks the student's attendance
-              for the day and assigns a color code (green for early, yellow for on
-              time, red for late).
-            </li>
-            <li>
-              Access Attendance Records and Digital Badges: Students, teachers, and
-              parents can view attendance records and digital badges on a dedicated
-              website.
-            </li>
-          </ul>
-        </div>
+                <div className="mt-2 text-sm text-gray-500">
+                  <ul>
+                    <li>
+                      <span class="bg-gradient-to-r text-xl from-blue-400 to-violet-400 text-transparent bg-clip-text">
+                        1. Scan QR Code
+                      </span>
+                    </li>
 
+                    <li>
+                      Students' unique QR codes are scanned at the school gate upon entering the school
+                    </li>
+                    <li>
+                      <span class="bg-gradient-to-r text-xl from-blue-400 to-violet-400 text-transparent bg-clip-text">
+                        2. Mark Attendance and Color Code:
+                      </span>
+                    </li>
+                    <li>
+                      The system marks the student's attendance for the day and
+                      assigns a color code (green for early, yellow for on time,
+                      red for late).
+                    </li>
+                    <li>
+                      Access Attendance Records and Digital Badges: Students,
+                      teachers, and parents can view attendance records and
+                      digital badges on a dedicated website.
+                    </li>
+                  </ul>
+                </div>
               )}
             </div>
           </div>
