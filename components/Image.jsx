64ca9f73945e575c64b7684abe7e8e447de1c237
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-05-08 14:23:06 +0000

    Mon, May 8, 2023, 2:23 PM +00:00

--- components/Image.jsx
+++ components/Image.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import Image from "next/image";
 
-
 const BobbingImage = () => {
   return (
-    <div className="h-40 w-40 flex items-center justify-center animate-bob">
-      <Image
-        src="/pictures/bob.png"
-        alt="Bobbing Image"
-        layout="fill"
-        />
+    <div className="flex items-center justify-center h-screen">
+      <div className="h-64 w-64 flex items-center justify-center animate-bob">
+        <div className="relative h-56 w-56">
+          <Image
+            src="/pictures/bob.png"
+            alt="Bobbing Image"
+            layout="fill"
+          />
+        </div>
+      </div>
     </div>
   );
 };
