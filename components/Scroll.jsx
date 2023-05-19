42ed2252979616802b29f6cb7ef2d8fa028a8d30
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-05-19 04:38:11 +0000

    Fri, May 19, 2023, 4:38 AM +00:00

--- components/Scroll.jsx
+++ components/Scroll.jsx
@@ -3,6 +3,27 @@ import React, { useState, useEffect } from "react";
 function ScrollToTopButton() {
   const [showButton, setShowButton] = useState(false);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const screenHeight = window.innerHeight;
+      const scrollPosition = window.pageYOffset;
+
+      // Calculate the threshold for showing the button (e.g., 20% of the screen height)
+      const threshold = screenHeight * 0.2;
+
+      // Update the showButton state based on the scroll position
+      setShowButton(scrollPosition > threshold);
+    };
+
+    // Add the scroll event listener
+    window.addEventListener("scroll", handleScroll);
+
+    // Cleanup: remove the scroll event listener
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
@@ -10,30 +31,17 @@ function ScrollToTopButton() {
     });
   };
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const isTallScreen = window.innerHeight < document.body.scrollHeight;
-      setShowButton(isTallScreen);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
-    <>
-      {showButton && (
-        <button
-          className="fixed bottom-4 right-4 bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full"
-          onClick={scrollToTop}
-        >
-          Scroll to Top
-        </button>
-      )}
-    </>
+    showButton && (
+      <button
+        className="fixed bottom-4 right-4 bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full"
+        onClick={scrollToTop}
+      >
+        Scroll to Top
+      </button>
+    )
   );
 }
 
 export default ScrollToTopButton;
+
