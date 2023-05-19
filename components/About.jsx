586d672d66db271583ae29c81a31baee1b965896
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-05-19 07:02:29 +0000

    Fri, May 19, 2023, 7:02 AM +00:00

--- components/About.jsx
+++ components/About.jsx
@@ -25,15 +25,17 @@ const About = () => {
 </div>
 
 <div class="text-center mt-8">
-  <h1 class="text-4xl font-bold text-gray-900">
-    With the widespread adoption of <span class="text-red-500">beep cards</span> in private schools,
+  <h1 class="text-lg font-bold text-gray-900">
+    <span class="text-gradient bg-gradient-to-r from-blue-400 to-violet-400 text-transparent bg-clip-text">
+      With the widespread adoption of beep cards in private schools,
+    </span>
   </h1>
-  <h1 class="text-4xl font-bold text-gray-900">
-    SCAS can <span class="text-blue-500">revolutionize</span> its attendance system by implementing its own <span class="text-green-500">innovative</span> solution.
+  <h1 class="text-lg font-bold text-gray-900">
+    <span class="text-gradient bg-gradient-to-r from-blue-400 to-violet-400 text-transparent bg-clip-text">
+      SCAS can revolutionize its attendance system by implementing its own innovative solution.
+    </span>
   </h1>
 </div>
-
-
 
         <div className="mt-6 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
           <div
