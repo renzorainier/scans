6df66e4367c04b91203a1fee99f144a3ac393277
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-05-19 04:23:09 +0000

    Fri, May 19, 2023, 4:23 AM +00:00

--- components/StudentWeek.jsx
+++ components/StudentWeek.jsx
@@ -5,20 +5,17 @@ function StudentChart({ student, onClose }) {
   const grades = ["Mon", "Tue", "Wed", "Thu", "Fri"];
   const times = ["A", "B", "C", "D", "E"];
 
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
+  const handleFloatingDivClick = () => {
+    window.scrollTo(0, 0);
   };
 
   return (
     <div>
       <div
         className="fixed z-50 top-0 left-0 w-full h-full backdrop-blur-xl rounded-lg"
-        onClick={scrollToTop}
+        onClick={handleFloatingDivClick}
       ></div>
-      <div className="fixed z-50 top-1/2 left-1/2  transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-md shadow-xl p-8 w-4/5 max-w-md">
+      <div className="fixed z-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-md shadow-xl p-8 w-4/5 max-w-md">
         {/* content here */}
       </div>
     </div>
