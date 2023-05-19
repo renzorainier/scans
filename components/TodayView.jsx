3a5c97825ac668ec536d85e4166188717400d8ed
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-05-19 02:52:06 +0000

    Fri, May 19, 2023, 2:52 AM +00:00

--- components/TodayView.jsx
+++ components/TodayView.jsx
@@ -128,13 +128,89 @@ function AttendanceTable() {
     setSearchQuery(event.target.value);
   };
 
-  if (isLoading) {
-    return <div></div>;
-  }
+  const tableData = selectedSection ? (
+    <tbody>
+      {filteredStudents.map((student, index) => (
+        <tr
+          className={`${
+            index % 2 === 0 ? "bg-gray-50" : "bg-gray-100"
+          } rounded-lg mb-2 py-2`}
+          key={student.studentId}
+          onClick={() => handleMoreClick(student)}
+        >
+          <td className="p-2">{filteredStudents.length - index}</td>
+          <td className="p-2 font-bold whitespace-nowrap">{student.name}</td>
+          <td className="p-2">{student.strand}</td>
+          <td className="p-2 ">{student.section}</td>
+          <td className="p-2 whitespace-nowrap">
+            {student.status === "late" && (
+              <div className="bg-[#EC7063] h-6 w-16 rounded-sm inline-block mr-1">
+                {student.lastScanTime}
+              </div>
+            )}
+            {student.status === "ontime" && (
+              <div className="bg-[#F7DC6F]  h-6 w-16 rounded-sm inline-block mr-1">
+                {student.lastScanTime}
+              </div>
+            )}
+            {student.status === "early" && (
+              <div className="bg-[#2ECC71]  h-6 w-16 rounded-sm inline-block mr-1">
+                {student.lastScanTime}
+              </div>
+            )}
+          </td>
+        </tr>
+      ))}
+      {selectedStudent && (
+        <StudentDetails student={selectedStudent} onClose={onClose} />
+      )}
+    </tbody>
+  ) : (
+    <tbody>
+      {presentStudents.map((student, index) => (
+        <tr
+          className={`${
+            index % 2 === 0 ? "bg-gray-50" : "bg-gray-100"
+          } rounded-lg mb-2 py-2`}
+          key={student.studentId}
+          onClick={() => handleMoreClick(student)}
+        >
+          <td className="p-2">{presentStudents.length - index}</td>
+          <td className="p-2 font-bold whitespace-nowrap">{student.name}</td>
+          <td className="p-2">{student.strand}</td>
+          <td className="p-2 ">{student.section}</td>
+          <td className="p-2 whitespace-nowrap">
+            {student.status === "late" && (
+              <div className="bg-[#EC7063] h-6 w-16 rounded-sm inline-block mr-1">
+                {student.lastScanTime}
+              </div>
+            )}
+            {student.status === "ontime" && (
+              <div className="bg-[#F7DC6F]  h-6 w-16 rounded-sm inline-block mr-1">
+                {student.lastScanTime}
+              </div>
+            )}
+            {student.status === "early" && (
+              <div className="bg-[#2ECC71]  h-6 w-16 rounded-sm inline-block mr-1">
+                {student.lastScanTime}
+              </div>
+            )}
+          </td>
+        </tr>
+      ))}
+      {selectedStudent && (
+        <StudentDetails student={selectedStudent} onClose={onClose} />
+      )}{" "}
+    </tbody>
+  );
 
   function handleHeaderClick(text) {
     setShowInfo(true);
     setInfoText(text);
+  }
+
+  if (isLoading) {
+    return <div></div>;
   }
 
   return (
@@ -289,52 +365,7 @@ function AttendanceTable() {
                           )}
                         </tr>
                       </thead>
-                      <tbody>
-                      
-                      </tbody>
-                      <tbody>
-                        {filteredStudents.map((student, index) => (
-                          <tr
-                            className={`${
-                              index % 2 === 0 ? "bg-gray-50" : "bg-gray-100"
-                            } rounded-lg mb-2 py-2`}
-                            key={student.studentId}
-                            onClick={() => handleMoreClick(student)}
-                          >
-                            <td className="p-2">
-                              {filteredStudents.length - index}
-                            </td>
-                            <td className="p-2 font-bold whitespace-nowrap">
-                              {student.name}
-                            </td>
-                            <td className="p-2">{student.strand}</td>
-                            <td className="p-2 ">{student.section}</td>
-                            <td className="p-2 whitespace-nowrap">
-                              {student.status === "late" && (
-                                <div className="bg-[#EC7063] h-6 w-16 rounded-sm inline-block mr-1">
-                                  {student.lastScanTime}
-                                </div>
-                              )}
-                              {student.status === "ontime" && (
-                                <div className="bg-[#F7DC6F]  h-6 w-16 rounded-sm inline-block mr-1">
-                                  {student.lastScanTime}
-                                </div>
-                              )}
-                              {student.status === "early" && (
-                                <div className="bg-[#2ECC71]  h-6 w-16 rounded-sm inline-block mr-1">
-                                  {student.lastScanTime}
-                                </div>
-                              )}
-                            </td>
-                          </tr>
-                        ))}
-                        {selectedStudent && (
-                          <StudentDetails
-                            student={selectedStudent}
-                            onClose={onClose}
-                          />
-                        )}
-                      </tbody>
+                      {tableData}
                     </table>
                     {showInfo && (
                       <div className="fixed z-50 top-0 left-0 w-screen h-screen backdrop-blur-xl bg-gray/90">
