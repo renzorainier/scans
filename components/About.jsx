9cfc151f41694afeedd9e8353045eed58fc6eb56
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-05-19 07:20:46 +0000

    Fri, May 19, 2023, 7:20 AM +00:00

--- components/About.jsx
+++ components/About.jsx
@@ -17,7 +17,7 @@ const About = () => {
 
   return (
     <div className="bg-gray-100">
-      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto py-8 px-4 sm:px-6 lg:px-8">
       <div class="text-center">
   <h2 class="text-3xl font-extrabold text-gray-900 sm:text-4xl">
     About <span class="bg-gradient-to-r from-blue-400 to-violet-400 text-transparent bg-clip-text">Scans</span>
