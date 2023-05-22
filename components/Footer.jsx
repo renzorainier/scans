ff6396d511b9cde3a5cbd13f4563ca72e935d19a
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-05-22 01:27:10 +0000

    Mon, May 22, 2023, 1:27 AM +00:00

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
     <footer className="bg-gray-200 py-4 px-8">
       <div className="container mx-auto text-center text-gray-500">
         <p className="text-sm">
-          &copy; {currentYear} Your Company. All rights reserved.
+          &copy; {currentYear} Scans. All rights reserved.
         </p>
       </div>
     </footer>
