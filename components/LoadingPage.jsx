20a572d10b5cb8e7f66d85b5056893cd706d6423
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-05-27 12:09:18 +0000

    Sat, May 27, 2023, 12:09 PM +00:00

--- components/LoadingPage.jsx
+++ components/LoadingPage.jsx
@@ -2,9 +2,10 @@ import React from "react";
 
 const LoadingPage = () => {
   return (
-    <div>
+    <div className="flex items-center justify-center h-screen">
       <video src="/pictures/intro.mp4" autoPlay />
     </div>
   );
 }
+
 export default LoadingPage;
