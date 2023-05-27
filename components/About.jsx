6bd1a74a03b135814a1fd1d60d0755870fa4832e
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-05-27 12:14:39 +0000

    Sat, May 27, 2023, 12:14 PM +00:00

--- components/About.jsx
+++ components/About.jsx
@@ -27,7 +27,7 @@ const About = () => {
             </span>
           </h2>
           <p className="text-gray-500 text-sm mt-2">
-            Latest Version: <span className="font-semibold">May 24, 2023</span>
+            Latest Version: <span className="font-semibold">May 27, 2023</span>
           </p>
         </div>
 
