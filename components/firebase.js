8e1e4a3645c14eeb8d1937162d08705a69c282bf
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-06-05 07:04:15 +0000

    Mon, Jun 5, 2023, 7:04 AM +00:00

--- components/firebase.js
+++ components/firebase.js
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_apiKey,
-  authDomain: process.env.NEXT_PUBLIC_authDomain,
-  projectId: process.env.NEXT_PUBLIC_projectId,
-  storageBucket: process.env.NEXT_PUBLIC_storageBucket,
-  messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
-  appId: process.env.NEXT_PUBLIC_appId
+  apiKey: "AIzaSyBFUy6OAoziQQQYnBr3TJVnbrlEDLBqsbQ",
+  authDomain: "qrcode-e0852.firebaseapp.com",
+  projectId: "qrcode-e0852",
+  storageBucket: "qrcode-e0852.appspot.com",
+  messagingSenderId: "964760739363",
+  appId: "1:964760739363:web:bee7a73ad557fd7458db96"
 };
 
 // Initialize Firebase
